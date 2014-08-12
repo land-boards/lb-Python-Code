29876dc50c6036086581d113a94873f07fe65965
Doug Gilliland <doug@douglasgilliland.com>
2014-08-11 21:04:30 -0400

    csv to mediawiki

---
+++ pyCSVtoMWTable/pyCSVtoMWTable/pyCSVtoMWTable.py
@@ -0,0 +1,199 @@
+"""
+pyCSVtoMWTable.py
+Convert a CSV into a MediaWiki table.
+
+"""
+
+import pygtk
+pygtk.require('2.0')
+
+import gtk
+
+# Check for new pygtk: this is new class in PyGtk 2.4
+if gtk.pygtk_version < (2,3,90):
+	 print "PyGtk 2.3.90 or later required"
+	 raise SystemExit
+
+import csv
+import string
+import sys
+import os
+
+from sys import argv
+
+def errorDialog(errorString):
+	"""
+	Prints an error message as a dialog box
+	"""
+	message = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK)
+	message.set_markup(errorString)
+	message.run()		# Display the dialog box and hang around waiting for the "OK" button
+	message.destroy()	# Takes down the dialog box
+	return
+			
+
+
+class FindCSVFile:
+	def findCSVFileBrowse(self, startingPath):
+		"""findCSVFileBrowse() - This is the dialog which locates the csv files
+	
+		:returns: path/name of the file that was selected
+		"""
+		csvFileString = "Select file"
+		dialog = gtk.FileChooserDialog(csvFileString,
+			None,
+			gtk.FILE_CHOOSER_ACTION_OPEN,
+			(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+			gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+		dialog.set_default_response(gtk.RESPONSE_OK)
+
+		if startingPath != '':
+			dialog.set_current_folder(startingPath)
+		filter = gtk.FileFilter()
+		filter.set_name("CSV files")
+		filter.add_pattern("*.csv")
+		dialog.add_filter(filter)
+
+		response = dialog.run()
+		if response == gtk.RESPONSE_OK:
+			retFileName = dialog.get_filename()
+			dialog.destroy()
+			return retFileName
+		elif response == gtk.RESPONSE_CANCEL:
+			print 'Closed, no files selected'
+			dialog.destroy()
+			exit()
+		dialog.destroy()
+
+class ControlClass:
+	def readInCSV(self, inFile):
+		csvReader = csv.reader(inFile)
+		list2Read = []
+		for row in csvReader:
+			list2Read.append(row)
+		return list2Read
+		
+	def writeOutMWTable(self, outFilePtr, theList):
+		outFilePtr.write('{| class="wikitable"\n')
+		firstRow = True
+		for row in theList:
+			for cell in row:
+				if firstRow:
+					outFilePtr.write('!' + cell + '\n')					
+				else:
+					outFilePtr.write('|' + cell + '\n')
+			firstRow = False
+			outFilePtr.write('|-\n')
+		outFilePtr.write('|}\n')
+
+	def theExecutive(self):
+		myCSV = FindCSVFile()
+		fileToRead = myCSV.findCSVFileBrowse('.')
+
+		fileToWrite = fileToRead[:-4] + ".MW"
+
+		try:
+			inFile = open(fileToRead, 'rb')
+		except IOError:
+			errorDialog('ERROR - Cannot open input file')
+			exit()
+		
+		try:
+			outFile = open(fileToWrite, 'wb')
+		except IOError:
+			errorDialog('ERROR - Cannot open the output file.\nIs the file already open?\nClose the file and return.')
+			try:
+				outFile = open(fileToWrite, 'wb')
+			except IOError:
+				errorDialog('ERROR - Tried again,  - Is the file already open?')
+				exit()
+
+		theInList = self.readInCSV(inFile)
+		
+		self.writeOutMWTable(outFile, theInList)
+
+class UIManager:
+	"""The UI manager
+	"""
+	interface = """
+	<ui>
+		<menubar name="MenuBar">
+			<menu action="File">
+				<menuitem action="Open"/>
+				<menuitem action="Quit"/>
+			</menu>
+			<menu action="Help">
+				<menuitem action="About"/>
+			</menu>
+		</menubar>
+	</ui>
+	"""
+
+	def __init__(self):
+		"""Initialize the class
+		"""
+		# Create the top level window
+		window = gtk.Window()
+		window.connect('destroy', lambda w: gtk.main_quit())
+		window.set_default_size(200, 200)
+		
+		vbox = gtk.VBox()
+		
+		# Create a UIManager instance
+		uimanager = gtk.UIManager()
+
+		# Add the accelerator group to the toplevel window
+		accelgroup = uimanager.get_accel_group()
+		window.add_accel_group(accelgroup)
+		window.set_title('kiPL - Kicad Parts List creation program')
+
+		# Create an ActionGroup
+		actiongroup =	gtk.ActionGroup("kiPL")
+		self.actiongroup = actiongroup
+
+		# Create actions
+		self.actiongroup.add_actions([
+									("Open", gtk.STOCK_OPEN, "_Open", None, "Open an Existing Document", self.openIF),
+									("Quit", gtk.STOCK_QUIT, "_Quit", None, "Quit the Application", self.quit_application),
+									("File", None, "_File"),
+									("Help", None, "_Help"),
+									("About", None, "_About", None, "About kiPL", self.about_kiPL),
+									])
+		uimanager.insert_action_group(self.actiongroup, 0)
+		uimanager.add_ui_from_string(self.interface)
+		
+		menubar = uimanager.get_widget("/MenuBar")
+		vbox.pack_start(menubar, False)
+		
+		window.connect("destroy", lambda w: gtk.main_quit())
+		
+		window.add(vbox)
+		window.show_all()
+
+	def openIF(self, b):
+		"""Open the interface by calling the control class
+		"""
+		myControl = ControlClass()
+		myControl.theExecutive()
+
+		message = gtk.MessageDialog(type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_OK)
+		message.set_markup("Conversion Complete")
+		message.run()		# Display the dialog box and hang around waiting for the "OK" button
+		message.destroy()	# Takes down the dialog box
+		return
+
+	def about_kiPL(self, b):
+		"""The about dialog
+		"""
+		message = gtk.MessageDialog(type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_OK)
+		message.set_markup("About kiPL\nAuthor: Doug Gilliland\n(c) 2014 - AAC - All rights reserved\nkiPL Process Deltek T and E Charge Account Report")
+		message.run()
+		message.destroy()
+		return
+
+	def quit_application(self, widget):
+		gtk.main_quit()
+
+if __name__ == '__main__':
+	ba = UIManager()
+	gtk.main()
--- pyCommonStuff/pyCommonStuff/pyCommonStuff.py
+++ pyCommonStuff/pyCommonStuff/pyCommonStuff.py
@@ -224,4 +224,90 @@ def errorDialog(errorString):
 	message.destroy()	# Takes down the dialog box
 	return
 			
+
 			
+class UIManager:
+	"""The UI manager
+	"""
+	interface = """
+	<ui>
+		<menubar name="MenuBar">
+			<menu action="File">
+				<menuitem action="Open"/>
+				<menuitem action="Quit"/>
+			</menu>
+			<menu action="Help">
+				<menuitem action="About"/>
+			</menu>
+		</menubar>
+	</ui>
+	"""
+
+	def __init__(self):
+		"""Initialize the class
+		"""
+		# Create the top level window
+		window = gtk.Window()
+		window.connect('destroy', lambda w: gtk.main_quit())
+		window.set_default_size(200, 200)
+		
+		vbox = gtk.VBox()
+		
+		# Create a UIManager instance
+		uimanager = gtk.UIManager()
+
+		# Add the accelerator group to the toplevel window
+		accelgroup = uimanager.get_accel_group()
+		window.add_accel_group(accelgroup)
+		window.set_title('kiPL - Kicad Parts List creation program')
+
+		# Create an ActionGroup
+		actiongroup =	gtk.ActionGroup("kiPL")
+		self.actiongroup = actiongroup
+
+		# Create actions
+		self.actiongroup.add_actions([
+									("Open", gtk.STOCK_OPEN, "_Open", None, "Open an Existing Document", self.openIF),
+									("Quit", gtk.STOCK_QUIT, "_Quit", None, "Quit the Application", self.quit_application),
+									("File", None, "_File"),
+									("Help", None, "_Help"),
+									("About", None, "_About", None, "About kiPL", self.about_kiPL),
+									])
+		uimanager.insert_action_group(self.actiongroup, 0)
+		uimanager.add_ui_from_string(self.interface)
+		
+		menubar = uimanager.get_widget("/MenuBar")
+		vbox.pack_start(menubar, False)
+		
+		window.connect("destroy", lambda w: gtk.main_quit())
+		
+		window.add(vbox)
+		window.show_all()
+
+	def openIF(self, b):
+		"""Open the interface by calling the control class
+		"""
+		myControl = ControlClass()
+		myControl.theExecutive()
+
+		message = gtk.MessageDialog(type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_OK)
+		message.set_markup("Conversion Complete")
+		message.run()		# Display the dialog box and hang around waiting for the "OK" button
+		message.destroy()	# Takes down the dialog box
+		return
+
+	def about_kiPL(self, b):
+		"""The about dialog
+		"""
+		message = gtk.MessageDialog(type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_OK)
+		message.set_markup("About kiPL\nAuthor: Doug Gilliland\n(c) 2014 - AAC - All rights reserved\nkiPL Process Deltek T and E Charge Account Report")
+		message.run()
+		message.destroy()
+		return
+
+	def quit_application(self, widget):
+		gtk.main_quit()
+
+if __name__ == '__main__':
+	ba = UIManager()
+	gtk.main()
