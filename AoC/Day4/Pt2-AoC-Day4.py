bbcfa3e57f9fe5bc381708e8b8ddcd2f8b5e5cd0
Doug Gilliland <doug@douglasgilliland.com>
2018-12-04 21:10:15 -0500

    Update Pt2-AoC-Day4.py

--- AoC/Day4/Pt2-AoC-Day4.py
+++ AoC/Day4/Pt2-AoC-Day4.py
@@ -9,41 +9,6 @@ import time
 import re
 
 """
---- Day 4: Repose Record ---
-You've sneaked into another supply closet - this time, it's across from the prototype suit manufacturing lab. You need to sneak inside and fix the issues with the suit, but there's a guard stationed outside the lab, so this is as close as you can safely get.
-
-As you search the closet for anything that might help, you discover that you're not the first person to want to sneak in. Covering the walls, someone has spent an hour starting every midnight for the past few months secretly observing this guard post! They've been writing down the ID of the one guard on duty that night - the Elves seem to have decided that one guard was enough for the overnight shift - as well as when they fall asleep or wake up while at their post (your puzzle input).
-
-For example, consider the following records, which have already been organized into chronological order:
-
-[1518-11-01 00:00] Guard #10 begins shift
-[1518-11-01 00:05] falls asleep
-[1518-11-01 00:25] wakes up
-[1518-11-01 00:30] falls asleep
-[1518-11-01 00:55] wakes up
-[1518-11-01 23:58] Guard #99 begins shift
-[1518-11-02 00:40] falls asleep
-[1518-11-02 00:50] wakes up
-[1518-11-03 00:05] Guard #10 begins shift
-[1518-11-03 00:24] falls asleep
-[1518-11-03 00:29] wakes up
-[1518-11-04 00:02] Guard #99 begins shift
-[1518-11-04 00:36] falls asleep
-[1518-11-04 00:46] wakes up
-[1518-11-05 00:03] Guard #99 begins shift
-[1518-11-05 00:45] falls asleep
-[1518-11-05 00:55] wakes up
-Timestamps are written using year-month-day hour:minute format. The guard falling asleep or waking up is always the one whose shift most recently started. Because all asleep/awake times are during the midnight hour (00:00 - 00:59), only the minute portion (00 - 59) is relevant for those events.
-
-Date   ID   Minute
-            000000000011111111112222222222333333333344444444445555555555
-            012345678901234567890123456789012345678901234567890123456789
-11-01  #10  .....####################.....#########################.....
-11-03  #10  ........................#####...............................
-11-02  #99  ........................................##########..........
-11-04  #99  ....................................##########..............
-11-05  #99  .............................................##########.....
-
 
 Strategy 2: Of all guards, which guard is most frequently asleep on the same minute?
 
@@ -64,7 +29,7 @@ def readTextFileToList(fileName):
 	#print 'Reading in file',time.strftime('%X %x %Z')
 	with open(fileName, 'r') as filehandle:  
 		for line in filehandle:
-			textFile.append(line.strip('\n\r'))
+			textFile.append(line.strip())
 		textFile.sort()
 	return textFile
 
