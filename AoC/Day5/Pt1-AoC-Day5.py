585d24c53285e978377442fa6c46124c8f17b833
Doug Gilliland <doug@douglasgilliland.com>
2018-12-05 06:40:02 -0500

    day 5

--- AoC/Day5/Pt1-AoC-Day5.py
+++ AoC/Day5/Pt1-AoC-Day5.py
@@ -1,21 +1,36 @@
 # Pt1-AoCDay4.py
 # 2018 Advent of Code
-# Day 4
-# Part 2
-# https://adventofcode.com/2018/day/4
+# Day 5
+# Part 1
+# https://adventofcode.com/2018/day/5
 
 import time
 import re
 
 """
 
-Strategy 2: Of all guards, which guard is most frequently asleep on the same minute?
+--- Day 5: Alchemical Reduction ---
+You've managed to sneak in to the prototype suit manufacturing lab. The Elves are making decent progress, but are still struggling with the suit's size reduction capabilities.
 
-In the example above, Guard #99 spent minute 45 asleep more than any other guard or minute - three times in total. (In all other cases, any guard spent any minute asleep at most twice.)
+While the very latest in 1518 alchemical technology might have solved their problem eventually, you can do better. You scan the chemical composition of the suit's material and discover that it is formed by extremely long polymers (one of which is available as your puzzle input).
 
-What is the ID of the guard you chose multiplied by the minute you chose? (In the above example, the answer would be 99 * 45 = 4455.)
+The polymer is formed by smaller units which, when triggered, react with each other such that two adjacent units of the same type and opposite polarity are destroyed. Units' types are represented by letters; units' polarity is represented by capitalization. For instance, r and R are units with the same type but opposite polarity, whereas r and s are entirely different types and do not react.
 
-Your puzzle answer was 7887.
+For example:
+
+In aA, a and A react, leaving nothing behind.
+In abBA, bB destroys itself, leaving aA. As above, this then destroys itself, leaving nothing.
+In abAB, no two adjacent units are of the same type, and so nothing happens.
+In aabAAB, even though aa and AA are of the same type, their polarities match, and so nothing happens.
+Now, consider a larger example, dabAcCaCBAcCcaDA:
+
+dabAcCaCBAcCcaDA  The first 'cC' is removed.
+dabAaCBAcCcaDA    This creates 'Aa', which is removed.
+dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
+dabCBAcaDA        No further actions can be taken.
+After all possible reactions, the resulting polymer contains 10 units.
+
+How many units remain after fully reacting the polymer you scanned? (Note: in this puzzle and others, the input is large; if you copy/paste your input, make sure you get the whole thing.)
 
 """
 
@@ -29,7 +44,6 @@ def readTextFileToList(fileName):
 	with open(fileName, 'r') as filehandle:  
 		for line in filehandle:
 			textFile.append(line.strip())
-		textFile.sort()
 	return textFile
 
 def parseGuardLog(guardLog):
