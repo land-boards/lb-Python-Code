210f33767e52c42b916a5cbf167ba078dad7cd11
Doug Gilliland <doug@douglasgilliland.com>
2018-12-10 09:58:04 -0500

    Update Pt1-AoC-Day8.py

--- AoC/Day8/Pt1-AoC-Day8.py
+++ AoC/Day8/Pt1-AoC-Day8.py
@@ -60,31 +60,47 @@ IndexError: list index out of range
 #####################################################################################
 ## Functions which read and form the input file
 
+inputList = []
+inFileListOff = 0
+
 class filer():
 	
-	inFileListOff = 0
-	inputList = []
 	
-	def loadList(self,filename):
-		readTextFileToList(filename):
+	def loadListFromFile(self,filename):
+		global inputList
+		inFileString = self.readTextFileToList(filename)
+		self.stringOfNumbersToList(inFileString)
+		inFileListOff = 0
+		print 'Input File Length =',len(inputList)
 
 	def getNextNodeFromList(self):
-		nextNode = [inputList[inFileListOff],inputList[inFileListOff+1],inputList[inFileListOff+2],inputList[inFileListOff+3]]
-		inFileListOff += 4
-		return [nextNode]
+		global inputList
+		global inFileListOff
+		debug_getNextNodeFromList = True
+		if debug_getNextNodeFromList:
+			print 'getNextNodeFromList: getting next node'
+		nextNode = [inputList[inFileListOff],inputList[inFileListOff+1]]
+		inFileListOff += 2
+		if debug_getNextNodeFromList:
+			print 'getNextNodeFromList: nextNode = ',nextNode
+		return nextNode
 	
+	def getFileOffset(self):
+		return inFileListOff
+
 	def readTextFileToList(self,fileName):
 		"""readTextFileAndSrtToList - open file and read the content to a list
 		File is sorted to produce a date/time ordered file
 		:returns: the list sorted list
 		"""
+		global inputList
 		textFile = ''
 		with open(fileName, 'r') as filehandle:  
 			for char in filehandle:
 				textFile += char
-		return True
+		return textFile
 		
-	def stringOfNumbersToList(str):
+	def stringOfNumbersToList(self,str):
 		"""stringOfNumbersToList - Take the input file which is a really long list and turn it into a python list
 		"""
 		num = 0
@@ -94,15 +110,103 @@ class filer():
 			else:
 				inputList.append(num)
 				num = 0
-		return theList
 
 #####################################################################################
 ## Functions which operate on the node list
 
+nodeList = []
+currentNodeNumber = 0
+UPNODENUM = 0
+DNNODENUM = 1
+LFNODENUM = 2
+RTNODENUM = 3
+NUMOFKIDS = 4
+METAOFFST = 5
+METALENGTH = 6
+FILEOFFST = 7
+
 class NodeFunctions():
 	
-	def addChild (self,):
+	
+	def createChildList(self):
+		return
+		
+	def addFirstNode(self,firstNode,inFileOffset):
+		"""addFirstNode - add the first node
+		Initialize the pointers for the first node
+		Special values for pointers are
+		-1 dead end (will not be updated later)
+		-2 uninitialized but will be later set to something
+		
+		:param nodeList: the pair [numberKids,lengthOfMetaData] from the input file
+		:returns: True when done
+		"""
+		global nodeList
+		global currentNodeNumber
+		debug_getNextNodeFromList = True
+		currentNodeNumber = 0
+		node = [0,0,0,0,0,0,0,0]
+		node[UPNODENUM] = -1
+		node[DNNODENUM] = -2
+		node[LFNODENUM] = -1
+		node[RTNODENUM] = -1
+		node[NUMOFKIDS] = firstNode[0]
+		node[METAOFFST] = -2
+		node[METALENGTH] = firstNode[1]
+		node[FILEOFFST] = inFileOffset
+		nodeList.append(node)
+		if debug_getNextNodeFromList:
+			print 'Added first node',node
+		return
+		
+	def processNode(self,nodeNumber):
+		"""processNode(nodeNumber) - Process the node
 
+		:param nodeNumber: The number of the current node
+		
+		"""
+		return False
+		
+	def dumpNodeVals(self,nodeNumber):
+		"""dumpNodeVals
+		
+		:param: nodeNumber the offset to the node in array
+		"""
+		debug_dumpNodeVals = True
+		print 'Dump of node Number',nodeNumber
+		if nodeNumber > len(nodeList) - 1:
+			print 'dumpNodeVals: ERROR - Offset is not in list, exiting...'
+			exit()
+		nodeVec = nodeList[nodeNumber]
+		if nodeVec[UPNODENUM] == -1:
+			print ' This is node 0'
+		else:
+			print ' Up node number',nodeVec[UPNODENUM]
+		if nodeVec[DNNODENUM] == -1:
+			print ' End point down'
+		elif nodeVec[DNNODENUM] == -2:
+			print ' Unsolved down'
+		else:
+			print ' First child node number',nodeVec[DNNODENUM]
+		if nodeVec[LFNODENUM] == -1:
+			print ' Nothing to the left'
+		elif nodeVec[LFNODENUM] == -2:
+			print ' Left uninitialized'
+		else:
+			print ' Left node',nodeVec[LFNODENUM]
+		if nodeVec[RTNODENUM] == -1:
+			print ' No sister to the right'
+		elif nodeVec[RTNODENUM] == -2:
+			print 'ERROR - uninitialized sister node - should never happen'
+			exit()
+		else:
+			print ' Sister node to the right',nodeVec[RTNODENUM]
+
+			
+
+def getCurrentNodeNumber():
+	return currentNodeNumber
+	
 ########################################################################
 ## Code
 
@@ -110,6 +214,21 @@ print 'Reading in file',time.strftime('%X %x %Z')
 
 inFileName = 'input2.txt'
 
-InputListHandler = filer():
-InputListHandler.loadList()
+InputListHandler = filer()
+InputListHandler.loadListFromFile(inFileName)
 
+NodeHandler = NodeFunctions()
+firstNode = InputListHandler.getNextNodeFromList()
+print 'firstNode is',firstNode
+
+inFileOff = InputListHandler.getFileOffset()	# should return 0
+NodeHandler.addFirstNode(firstNode,inFileOff)
+NodeHandler.dumpNodeVals(getCurrentNodeNumber())
+
+while True:
+	inFileOff = InputListHandler.getFileOffset()
+	currentNode = getCurrentNodeNumber()
+	if not NodeHandler.processNode(inFileOff):
+		break
+
+print 'done'
