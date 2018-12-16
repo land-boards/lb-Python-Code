782b2ac96a85ca297aef788f119d1247b86d8541
Doug Gilliland <doug@douglasgilliland.com>
2018-12-16 09:18:06 -0500

    moved stuff around after getting part 1

--- AoC/Day8/Pt2-AoC-Day8.py
+++ AoC/Day8/Pt2-AoC-Day8.py
@@ -5,63 +5,34 @@
 # https://adventofcode.com/2018/day/8
 
 import time
-from datetime import datetime, timedelta
 import re
 import os
 """
+--- Part Two ---
+The second check is slightly more complicated: you need to find the value of the root node (A in the example above).
 
---- Day 8: Memory Maneuver ---
-The sleigh is much easier to pull than you'd expect for something its weight. Unfortunately, neither you nor the Elves know which way the North Pole is from here.
+The value of a node depends on whether it has child nodes.
 
-You check your wrist device for anything that might help. It seems to have some kind of navigation system! Activating the navigation system produces more bad news: "Failed to start navigation system. Could not read software license file."
+If a node has no child nodes, its value is the sum of its metadata entries. So, the value of node B is 10+11+12=33, 
+and the value of node D is 99.
 
-The navigation system's license file consists of a list of numbers (your puzzle input). The numbers define a data structure which, when processed, produces some kind of tree that can be used to calculate the license number.
+However, if a node does have child nodes, the metadata entries become indexes which refer to those child nodes. 
+A metadata entry of 1 refers to the first child node, 2 to the second, 3 to the third, and so on. 
+The value of this node is the sum of the values of the child nodes referenced by the metadata entries. 
+If a referenced child node does not exist, that reference is skipped. 
+A child node can be referenced multiple time and counts each time it is referenced. 
+A metadata entry of 0 does not refer to any child node.
 
-The tree is made up of nodes; a single, outermost node forms the tree's root, and it contains all other nodes in the tree (or contains nodes that contain nodes, and so on).
+For example, again using the above nodes:
 
-Specifically, a node consists of:
+Node C has one metadata entry, 2. Because node C has only one child node, 
+2 references a child node which does not exist, and so the value of node C is 0.
+Node A has three metadata entries: 1, 1, and 2. The 1 references node A's first child node, B, 
+and the 2 references node A's second child node, C. 
+Because node B has a value of 33 and node C has a value of 0, the value of node A is 33+33+0=66.
+So, in this example, the value of the root node is 66.
 
-A header, which is always exactly two numbers:
-The quantity of child nodes.
-The quantity of metadata entries.
-Zero or more child nodes (as specified in the header).
-One or more metadata entries (as specified in the header).
-Each child node is itself a node that has its own header, child nodes, and metadata. For example:
-
-0 0 0 0  0  0  0 0 0 0 1  1 1 1 1 1
-0 1 2 3  4  5  6 7 8 9 0  1 2 3 4 5
-
-2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
-A----------------------------------
-    B----------- C-----------
-                     D-----
-In this example, each node of the tree is also marked with an underline starting with a letter for easier identification. In it, there are four nodes:
-
-A, which has 2 child nodes (B, C) and 3 metadata entries (1, 1, 2).
-B, which has 0 child nodes and 3 metadata entries (10, 11, 12).
-C, which has 1 child node (D) and 1 metadata entry (2).
-D, which has 0 child nodes and 1 metadata entry (99).
-The first check done on the license file is to simply add up all of the metadata entries. In this example, that sum is 1+1+2+10+11+12+2+99=138.
-
-What is the sum of all metadata entries?
-
-Note: It's really more like this
-
-0 0 0 0  0  0  0 0 0 0 1  1 1 1 1 1
-0 1 2 3  4  5  6 7 8 9 0  1 2 3 4 5
-
-2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
-A--                           -----
-    B----------- C--       --
-                     D-----
-
-2044 nodes
-
-Traceback (most recent call last):
- line 98, in pushNodeAtPointToUnsolvedList
-    currentMetaCount = myList[listOffset + 1]
-IndexError: list index out of range
-
+What is the value of the root node?
 """
 
 debugAllModules = False
@@ -276,7 +247,7 @@ class NodeFunctions():
 			debug_pushNode = True
 		else:
 			debug_pushNode = False
-		#print '.',		# print a dot for every node that gets pushed - fairly low overhead cost
+		print '.',		# print a dot for every node that gets pushed - fairly low overhead cost
 		if len(nodeList) > stopLength:		# stop if the list runs away
 			self.dumpAllNodeVals()
 			exit()
@@ -972,6 +943,7 @@ def sumTheMetaStuff():
 	for node in nodeList:
 		startSpan = node[METAOFFST]
 		endSpan = node[METAOFFST] + node[METALENGTH]
+#		print startSpan,endSpan-1
 		while(startSpan < endSpan):
 			accumMetaRecLens += inputList[startSpan]
 			startSpan += 1
@@ -989,25 +961,15 @@ InputListHandler.loadListFromFile(inFileName)
 
 print 'Processing Nodes',time.strftime('%X %x %Z')
 
-now = datetime.now()
-startuS = now.microsecond
-print startuS
-
 NodeHandler = NodeFunctions()
 
 newCoreCode()
 
-end = datetime.now()
-enduS = end.microsecond
-print enduS
-print 'microseconds',enduS - startuS
-
 print 'main: processing is done'
-#print 'node at the end was',currentNodeNumber
-#NodeHandler.dumpAllNodeVals()
+print 'node at the end was',currentNodeNumber
+NodeHandler.dumpAllNodeVals()
 
 sumTheMetaStuff()
-print 'Completed',time.strftime('%X %x %Z')
 
 exit()
 
