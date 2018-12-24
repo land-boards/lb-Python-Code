10474877418643554fa937fb8e9375e37e1396cb
Doug Gilliland <doug@douglasgilliland.com>
2018-12-24 15:48:00 -0500

    In the middle of this one.

--- AoC/Day14/Pt1-AoC-Day14.py
+++ AoC/Day14/Pt1-AoC-Day14.py
@@ -73,49 +73,6 @@ Your puzzle input is 110201.
 """
 
 #####################################################################################
-## Functions which operate on the input file and node lists
-
-class InputFileHandler():
-
-	def readTextFileLinesToList(self,fileName):
-		"""readTextFileAndSrtToList - open file and read the content to a list
-		:returns: the list
-		"""
-		textFile = ''
-		with open(fileName, 'r') as filehandle:  
-			textFile = filehandle.readlines()
-		inList = []
-		for row in textFile:
-			inList.append(row.strip('\n\r'))
-		return inList
-			
-	def writeOutMapFile(self,mapList):
-		"""writeOutMapFile - Write out the map file so that it can be read by an editor.
-		The map file is too big to print in a 80 xValueNum DOS CMD window.
-		newline between each line
-		"""
-		mapAsList = self.mapToList(mapList)
-		with open('SnapMap.txt', 'w') as f:
-			for item in mapAsList:
-				f.write(item)
-				f.write('\n')
-		
-	def mapToList(self,mapList):
-		"""Write out the mapList to a file because it is too big to see on the screen
-		"""
-		debug_mapToList = False
-		if debug_mapToList:
-			print 'writeOutMapFile: newLine',mapList[0]
-			print 'writeOutMapFile: mapList has line count',len(mapList)
-		outList = []
-		for line in mapList:
-			newLine = ''.join(line)
-			outList.append(newLine)
-		if debug_mapToList:
-			print 'mapToList: outList',
-		return outList
-
-#####################################################################################
 ## Functions which deal in general with programming tasks
 
 def abbyTerminate(string):
@@ -125,128 +82,7 @@ def abbyTerminate(string):
 	exit()
 
 #####################################################################################
-
-def makeEmptyMap(theMap):
-	"""Make a copy of the map without anything in it.
-	"""
-	yValueNumCount = len(theMap)
-	xValueNumCount = len(theMap[0])
-	newMap = []
-	for yValueNum in xrange(yValueNumCount):
-		xList = []
-		for xValueNum in xrange(xValueNumCount):
-			xList.append('0')
-		newMap.append(xList)
-	return newMap
-
-#####################################################################################
-## Functions which operate on the map list
-
-def makeMapArray(theTextList):
-	"""Go through the input list and make an array from the lines of textFile
-	
-	:param theTextList: The text file as a list of strings. 
-	Each string is a line of the file.
-	:returns: map turned into 2D list (more or less an array) 
-	"""
-	debug_makeMapArray = False
-	if debug_makeMapArray:
-		print 'makeMapArray: make an array from the text lines'
-	mapArray = []
-	for yValueNum in theTextList:
-		yValueNumList = list(yValueNum)
-		mapArray.append(yValueNumList)
-	return mapArray
-	
-def determineNextSymbols(map):
-	newMap = makeEmptyMap(map)	# copy the current map size as a new map
-	xValueNumCount = len(map[0])
-	yValueNumCount = len(map)
-	for yValueNum in xrange(yValueNumCount):
-		for xValueNum in xrange(xValueNumCount):
-			if map[yValueNum][xValueNum] == '.':		# Open Ground
-				if countObjectsAround('|',xValueNum,yValueNum,map) >= 3:
-					newMap[yValueNum][xValueNum] = '|'
-				else:
-					newMap[yValueNum][xValueNum] = map[yValueNum][xValueNum]
-			elif map[yValueNum][xValueNum] == '|':	# Trees
-				if countObjectsAround('#',xValueNum,yValueNum,map) >= 3:
-					newMap[yValueNum][xValueNum] = '#'
-				else:
-					newMap[yValueNum][xValueNum] = map[yValueNum][xValueNum]
-			elif map[yValueNum][xValueNum] == '#':	# Lumberyard
-				if countObjectsAround('#',xValueNum,yValueNum,map) >= 1 and countObjectsAround('|',xValueNum,yValueNum,map) >= 1:
-					newMap[yValueNum][xValueNum] = '#'
-				else:
-					newMap[yValueNum][xValueNum] = '.'
-	return newMap
-
-def countObjectsAround(lookingForChar,xValueNum,yValueNum,map):
-	debug_countObjectsAround = False
-	charCount = 0
-	if getVal(-1,-1,xValueNum,yValueNum,map) == lookingForChar:
-		charCount += 1
-	if getVal(0,-1,xValueNum,yValueNum,map) == lookingForChar:
-		charCount += 1
-	if getVal(1,-1,xValueNum,yValueNum,map) == lookingForChar:
-		charCount += 1
-	if getVal(-1,0,xValueNum,yValueNum,map) == lookingForChar:
-		charCount += 1
-	if getVal(1,0,xValueNum,yValueNum,map) == lookingForChar:
-		charCount += 1
-	if getVal(-1,1,xValueNum,yValueNum,map) == lookingForChar:
-		charCount += 1
-	if getVal(0,1,xValueNum,yValueNum,map) == lookingForChar:
-		charCount += 1
-	if getVal(1,1,xValueNum,yValueNum,map) == lookingForChar:
-		charCount += 1
-	if debug_countObjectsAround:
-		print 'countObjectsAround: at x y',xValueNum,yValueNum,'counted',charCount,'of char',lookingForChar
-	return charCount
-
-def getVal(xOffset,yOffset,xValueNum,yValueNum,map):
-	"""getVal - Get the value of the character at a position with offset.
-	
-	:param xOffset:
-	:param yOffset:
-	:param xValueNum:
-	:param yValueNum:
-	:param map:	
-	:returns: the character if in the map, otherwise char zero '0'
-	"""
-	if (xValueNum + xOffset) < 0:
-		return '0'
-	if (xValueNum + xOffset) > (len(map[0]) - 1):
-		return '0'
-	if (yValueNum + yOffset) < 0:
-		return '0'
-	if (yValueNum + yOffset) > (len(map[0]) - 1):
-		return '0'
-	return map[yValueNum+yOffset][xValueNum+xOffset]
-
-def dumpMapList(map):
-	"""Dump the elf list
-	"""
-	print 'dumpMapList:'
-	xValueNumCount = len(map[0])
-	yValueNumCount = len(map)
-	for yValueNum in xrange(yValueNumCount):
-		for xValueNum in range(xValueNumCount):
-			print map[yValueNum][xValueNum],
-		print
-
-def valueForest(forestMap):
-	numberOfTrees = 0
-	numberOfLumbermills = 0
-	xValueNumCount = len(forestMap[0])
-	yValueNumCount = len(forestMap)
-	for yValueNum in xrange(yValueNumCount):
-		for xValueNum in xrange(xValueNumCount):
-			if forestMap[yValueNum][xValueNum] == '|':
-				numberOfTrees += 1
-			elif forestMap[yValueNum][xValueNum] == '#':
-				numberOfLumbermills += 1
-	return numberOfTrees*numberOfLumbermills
+##
 
 ########################################################################
 ## This is the workhorse of this assignment
@@ -255,27 +91,26 @@ def valueForest(forestMap):
 ########################################################################
 ## Code
 
-inFileName = 'input.txt'
+print 'Starting Processing',time.strftime('%X %x %Z')
 
-debug_main = False
-print 'Reading in file',time.strftime('%X %x %Z')
-InputFileClass = InputFileHandler()
-textList = InputFileClass.readTextFileLinesToList(inFileName)
-if debug_main:
-	print '\ntextList',textList
-forestMap = makeMapArray(textList)				# Get the map from the file
-dumpMapList(forestMap)
-loopCount = 1
-while loopCount <= 10:
-	print 'After',loopCount,'mins'
-	newForestMap = determineNextSymbols(forestMap)
-	dumpMapList(newForestMap)
-	forestMap = newForestMap
+elf1Offset = 0
+elf2Offset = 1
+
+recipeList = [3,7]
+loopCount = 0
+while loopCount < 10:
+	print loopCount
+	newRecipe = recipeList[elf1Offset] + recipeList[elf2Offset]
+	if newRecipe >= 10:
+		recipe10sDigit = newRecipe/10
+		recipe1sDigit = newRecipe - (10*newRecipe)
+		recipeList.append(recipe10sDigit)
+		recipeList.append(recipe1sDigit)
+	else:
+		recipeList.append(newRecipe)
+	listLength = len(recipeList)
+	print 'length of recipe list',listLength
+	print 'recipeList =',recipeList
 	loopCount += 1
-	
-forestValue = valueForest(forestMap)
-
-print 'forest value is',forestValue
-
 
 print 'Finished processing',time.strftime('%X %x %Z')
