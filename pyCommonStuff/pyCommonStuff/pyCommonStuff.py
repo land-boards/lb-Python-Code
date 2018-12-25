4a7d70e2336e17e1a4bc8d7e288d95ed881d4214
Doug Gilliland <doug@douglasgilliland.com>
2018-12-25 09:16:17 -0500

    Added scatterplots functions

--- pyCommonStuff/pyCommonStuff/pyCommonStuff.py
+++ pyCommonStuff/pyCommonStuff/pyCommonStuff.py
@@ -2,6 +2,26 @@
 """
 
 #################################################################################
+##  plot the map
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plotScatterPoints(xyList):
+	x = []
+	y = []
+	for point in xyList:
+		x.append(point[0])
+		y.append(point[1])
+
+	plt.scatter(x,y)
+	plt.show()
+
+xyList = [[1,1],[3,2],[5,2]]
+	
+plotScatterPoints(xyList)
+
+#################################################################################
 ## map
 
 def integerize(str):
