8136c83901382bb0f83c29548a1ba6c4f8bd2c93
Doug Gilliland <doug@douglasgilliland.com>
2018-12-25 13:16:49 -0500

    Extend Day 11 to nXn array sum function

---
+++ AoC/Day11/Pt2-AoC-Day11.py
@@ -0,0 +1,187 @@
+# Pt1-AoCDay11.py
+# 2018 Advent of Code
+# Day 11
+# Part 1
+# https://adventofcode.com/2018/day/11
+
+import time
+import re
+import os
+
+"""
+
+--- Day 11: Chronal Charge ---
+You watch the Elves and their sleigh fade into the distance as they head toward the North Pole.
+
+Actually, you're the one fading. 
+The falling sensation returns.
+
+The low fuel warning light is illuminated on your wrist-mounted device. 
+Tapping it once causes it to project a hologram of the situation: a 300x300 grid of fuel cells 
+and their current power levels, some negative. 
+You're not sure what negative power means in the context of time travel, but it can't be good.
+
+Each fuel cell has a coordinate ranging from 1 to 300 in both the X (horizontal) 
+and Y (vertical) direction. 
+In X,Y notation, the top-left cell is 1,1, and the top-right cell is 300,1.
+
+The interface lets you select any 3x3 square of fuel cells. 
+To increase your chances of getting to your destination, 
+you decide to choose the 3x3 square with the largest total power.
+
+The power level in a given fuel cell can be found through the following process:
+
+Find the fuel cell's rack ID, which is its X coordinate plus 10.
+Begin with a power level of the rack ID times the Y coordinate.
+Increase the power level by the value of the grid serial number (your puzzle input).
+Set the power level to itself multiplied by the rack ID.
+Keep only the hundreds digit of the power level (so 12345 becomes 3; 
+numbers with no hundreds digit become 0).
+Subtract 5 from the power level.
+For example, to find the power level of the fuel cell at 3,5 in a grid with serial number 8:
+
+The rack ID is 3 + 10 = 13.
+The power level starts at 13 * 5 = 65.
+Adding the serial number produces 65 + 8 = 73.
+Multiplying by the rack ID produces 73 * 13 = 949.
+The hundreds digit of 949 is 9.
+Subtracting 5 produces 9 - 5 = 4.
+So, the power level of this fuel cell is 4.
+
+Here are some more example power levels:
+
+Fuel cell at  122,79, grid serial number 57: power level -5.
+Fuel cell at 217,196, grid serial number 39: power level  0.
+Fuel cell at 101,153, grid serial number 71: power level  4.
+Your goal is to find the 3x3 square which has the largest total power. 
+The square must be entirely within the 300x300 grid. 
+Identify this square using the X,Y coordinate of its top-left fuel cell. 
+For example:
+
+For grid serial number 18, the largest total 3x3 square has a top-left corner of 33,45 
+(with a total power of 29); these fuel cells appear in the middle of this 5x5 region:
+
+-2  -4   4   4   4
+-4   4   4   4  -5
+ 4   3   3   4  -4
+ 1   1   2   4  -3
+-1   0   2  -5  -2
+For grid serial number 42, the largest 3x3 square's top-left is 21,61 
+(with a total power of 30); they are in the middle of this region:
+
+-3   4   2   2   2
+-4   4   3   3   4
+-5   3   3   4  -4
+ 4   3   3   4  -3
+ 3   3   3  -5  -1
+What is the X,Y coordinate of the top-left fuel cell of the 3x3 square 
+with the largest total power?
+
+Your puzzle input is 2866.
+
+Your puzzle answer was 20,50.
+
+That's the right answer! 
+You are one gold star closer to fixing the time stream.
+
+"""
+
+#####################################################################################
+## Functions which deal in general with programming tasks
+
+def abbyTerminate(string):
+	"""Terminate program due to abnormal condition
+	"""
+	print 'ERROR Terminating due to',string
+	exit()
+
+#####################################################################################
+##
+
+def make2dList(ySize,xSize):
+	"""make2dList - Make a 2D list
+	"""
+	a=[]
+	for row in xrange(ySize): a += [[0]*(xSize)]
+	return a
+	
+def clearArray(arrayToClear,fillValue=0):
+	"""clearArray - Fill 2D square array with -1 values
+	"""
+	for y in range(len(arrayToClear)):
+		for x in range(len(arrayToClear[0])):
+			arrayToClear[y][x] = fillValue
+	return arrayToClear
+	
+def setFuelLevels(fuelCellArrayAs2DList):
+	for yOffset in xrange(1,len(fuelCellArrayAs2DList)):
+		for xOffset in xrange(1,len(fuelCellArrayAs2DList[0])):
+			rackID = xOffset + 10
+			powerLevel = (rackID * yOffset) + gridSerialNumber
+			powerLevel = powerLevel * rackID
+			#print 'xy',xOffset,yOffset,'powerLevel',powerLevel
+			if powerLevel < 100:
+				powerLevel = 0
+			else:
+				powerString = str(powerLevel)
+				#print 'powerString',powerString
+				power100sDigitString = str(powerString)
+				#print 'power100sDigitString',power100sDigitString
+				powerLevel = int(power100sDigitString[-3])
+			powerLevel -= 5
+			#print 'powerLevel',powerLevel
+			fuelCellArrayAs2DList[yOffset][xOffset] = powerLevel
+	return fuelCellArrayAs2DList
+
+def dumpFuelCellArray(fuelCellArrayAs2DList):
+	for yOffset in xrange(1,len(fuelCellArrayAs2DList)):
+		for xOffset in xrange(1,len(fuelCellArrayAs2DList[0])):
+			print fuelCellArrayAs2DList[yOffset][xOffset],
+		print
+
+def get3x3Power(xOffset,yOffset,fuelCellArrayAs2DList):
+	totalPower = fuelCellArrayAs2DList[yOffset][xOffset]
+	totalPower += fuelCellArrayAs2DList[yOffset][xOffset+1]
+	totalPower += fuelCellArrayAs2DList[yOffset][xOffset+2]
+	totalPower += fuelCellArrayAs2DList[yOffset+1][xOffset]
+	totalPower += fuelCellArrayAs2DList[yOffset+1][xOffset+1]
+	totalPower += fuelCellArrayAs2DList[yOffset+1][xOffset+2]
+	totalPower += fuelCellArrayAs2DList[yOffset+2][xOffset]
+	totalPower += fuelCellArrayAs2DList[yOffset+2][xOffset+1]
+	totalPower += fuelCellArrayAs2DList[yOffset+2][xOffset+2]
+	return totalPower
+
+def findLargestPowerGrid(fuelCellArrayAs2DList):
+	maxPower = get3x3Power(1,1,fuelCellArrayAs2DList)
+	maxXY = [1,1]
+	for yOffset in xrange(1,len(fuelCellArrayAs2DList)-2):
+		for xOffset in xrange(1,len(fuelCellArrayAs2DList[0])-2):
+			cells3x3Power = get3x3Power(xOffset,yOffset,fuelCellArrayAs2DList)
+			#print 'xy,power',xOffset,yOffset,cells3x3Power
+			if cells3x3Power > maxPower:
+				maxPower = cells3x3Power
+				maxXY = [xOffset,yOffset]
+	return maxXY
+
+########################################################################
+## This is the workhorse of this assignment
+
+
+########################################################################
+## Code
+
+#gridSerialNumber = 18
+gridSerialNumber = 2866	# The program input
+
+print 'Starting Processing',time.strftime('%X %x %Z')
+
+fuelCellArrayAs2DList = make2dList(301,301)
+clearArray(fuelCellArrayAs2DList,0)
+setFuelLevels(fuelCellArrayAs2DList)
+#dumpFuelCellArray(fuelCellArrayAs2DList)
+#print 'power at 101,153 is',fuelCellArrayAs2DList[153][101]
+#exit()
+maxXY = findLargestPowerGrid(fuelCellArrayAs2DList)
+print 'maxXY is at',maxXY
+
+print 'Finished processing',time.strftime('%X %x %Z')
