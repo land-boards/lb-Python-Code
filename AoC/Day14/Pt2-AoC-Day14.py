c069fb24b1a519481c6ccd4b1d1c40580ae4a6fd
Doug Gilliland <doug@douglasgilliland.com>
2019-01-06 19:14:26 -0500

    Solved with an ob1

--- AoC/Day14/Pt2-AoC-Day14.py
+++ AoC/Day14/Pt2-AoC-Day14.py
@@ -21,6 +21,12 @@ They actually want to know how many recipes appear on the scoreboard to the left
 
 How many recipes appear on the scoreboard to the left of the score sequence in your puzzle input?
 
+Your puzzle answer was 20291131.
+
+That's the right answer! You are one gold star closer to fixing the time stream.
+
+Code below produces a result 1 too high.
+
 """
 
 #####################################################################################
@@ -38,21 +44,21 @@ def abbyTerminate(string):
 ########################################################################
 ## This is the workhorse of this assignment
 
-def checkForEndCondition(endString,recipeList):
+def checkForEndCondition(endString,recipeList,ob1):
 	debug_checkForEndCondition = False
-	if debug_checkForEndCondition:
-		print '\ncheckForEndCondition: reached function'
 	if len(recipeList) < len(endString):
 		if debug_checkForEndCondition:
 			print 'checkForEndCondition: too short to compare'
 		return False
-	offsetToLastRecipeCharacters = len(recipeList) - len(endString)
+	offsetToLastRecipeCharacters = len(recipeList) - len(endString) + ob1
 	if debug_checkForEndCondition:
+		print '\ncheckForEndCondition: reached function ob1',ob1
 		print 'checkForEndCondition: Checking for endString',endString
 		print 'checkForEndCondition: recipe list',recipeList
 		print 'checkForEndCondition: offsetToLastRecipeCharacters',offsetToLastRecipeCharacters
 	for offset in xrange(len(endString)):
-		#print 'checkForEndCondition: comparing',endString[offset],'to',recipeList[offsetToLastRecipeCharacters]
+		if debug_checkForEndCondition:
+			print 'checkForEndCondition: comparing',endString[offset],'to',recipeList[offsetToLastRecipeCharacters]
 		if int(endString[offset]) != int(recipeList[offsetToLastRecipeCharacters]):
 			if debug_checkForEndCondition:
 				print 'checkForEndCondition: mismatch',offsetToLastRecipeCharacters
@@ -62,38 +68,57 @@ def checkForEndCondition(endString,recipeList):
 		print 'checkForEndCondition: matched'
 	return True
 
+def findPatternInRecipes(lookingForString):
+	"""findPatternInRecipes - Make the recipes and call function to look for end condition
+	
+	:param lookingForString: The test string I am looking for
+	"""
+	elf1Offset = 0
+	elf2Offset = 1
+
+	recipeList = [3,7]		# seed of the list
+	listLength = 2
+	while True:
+		newRecipe = recipeList[elf1Offset] + recipeList[elf2Offset]
+		if newRecipe < 10:
+			recipeList.append(newRecipe)
+			listLength += 1
+		else:
+			recipeList.append(newRecipe/10)
+			recipeList.append(newRecipe % 10)
+			listLength += 2
+		elf1Offset = (elf1Offset + 1 + recipeList[elf1Offset]) % listLength
+		elf2Offset = (elf2Offset + 1 + recipeList[elf2Offset]) % listLength
+
+		if checkForEndCondition(lookingForString,recipeList,0):
+			print 'Input',lookingForString,'reached at',listLength-len(lookingForString),'recipes'
+			return listLength-len(lookingForString)
+
+		if checkForEndCondition(lookingForString,recipeList,-1):
+			print 'Input',lookingForString,'reached at',listLength-len(lookingForString),'recipes'
+			return listLength-len(lookingForString)-1
+
 ########################################################################
 ## Code
 
 print 'Starting Processing',time.strftime('%X %x %Z')
 
-elf1Offset = 0
-elf2Offset = 1
-
-#lookingForString = '51589'
-#lookingForString = '01245'
-#lookingForString = '59414'
-#lookingForString = '92510'
 lookingForString = '110201'
 
-recipeList = [3,7]
-keepLooping = True
-while keepLooping:
-	newRecipe = recipeList[elf1Offset] + recipeList[elf2Offset]
-	if newRecipe >= 10:
-		recipe10sDigit = newRecipe/10
-		recipe1sDigit = newRecipe % 10
-		recipeList.append(recipe10sDigit)
-		recipeList.append(recipe1sDigit)
-	else:
-		recipeList.append(newRecipe)
-	listLength = len(recipeList)
-	elf1Offset = (elf1Offset + 1 + recipeList[elf1Offset]) % listLength
-	elf2Offset = (elf2Offset + 1 + recipeList[elf2Offset]) % listLength
-
-	if checkForEndCondition(lookingForString,recipeList):
-		print 'Input',lookingForString,'reached at',listLength-len(lookingForString),'recipes'
-		break
+if findPatternInRecipes('01245') != 5:
+	print 'main: test case 01245 failed'
+	exit()
+if findPatternInRecipes('51589') != 9:
+	print 'main: test case 51589 failed'
+	exit()
+if findPatternInRecipes('59414') != 2018:
+	print 'main: test case 59414 failed'
+	exit()
+if findPatternInRecipes('92510') != 18:
+	print 'main: test case 92510 failed'
+	exit()
+print 'main: all example tests passed'
+findPatternInRecipes('110201')
 
 print 'Finished processing',time.strftime('%X %x %Z')
 
