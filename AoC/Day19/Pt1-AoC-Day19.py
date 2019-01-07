1902bb233c5f3ca0aa2392fb34fde86ff77dc014
Doug Gilliland <doug@douglasgilliland.com>
2019-01-07 15:03:23 -0500

    Update Pt1-AoC-Day19.py

--- AoC/Day19/Pt1-AoC-Day19.py
+++ AoC/Day19/Pt1-AoC-Day19.py
@@ -9,6 +9,93 @@ import re
 import os
 
 """
+--- Day 19: Go With The Flow ---
+With the Elves well on their way constructing the North Pole base, 
+you turn your attention back to understanding the inner workings of programming the device.
+
+You can't help but notice that the device's opcodes don't contain any flow control like jump instructions. 
+The device's manual goes on to explain:
+
+"In programs where flow control is required, the instruction pointer can be bound to a register 
+so that it can be manipulated directly. 
+This way, setr/seti can function as absolute jumps, addr/addi can function as relative jumps, 
+and other opcodes can cause truly fascinating effects."
+
+This mechanism is achieved through a declaration like #ip 1, 
+which would modify register 1 so that accesses to it let the program indirectly access the 
+instruction pointer itself. 
+To compensate for this kind of binding, there are now six registers (numbered 0 through 5); 
+the five not bound to the instruction pointer behave as normal. 
+Otherwise, the same rules apply as the last time you worked with this device.
+
+When the instruction pointer is bound to a register, its value is written to that register 
+just before each instruction is executed, and the value of that register is written back 
+to the instruction pointer immediately after each instruction finishes execution. 
+Afterward, move to the next instruction by adding one to the instruction pointer, 
+even if the value in the instruction pointer was just updated by an instruction. 
+(Because of this, instructions must effectively set the instruction pointer to the 
+instruction before the one they want executed next.)
+
+The instruction pointer is 0 during the first instruction, 1 during the second, and so on. 
+If the instruction pointer ever causes the device to attempt to load an instruction outside 
+the instructions defined in the program, the program instead immediately halts. 
+The instruction pointer starts at 0.
+
+It turns out that this new information is already proving useful: 
+the CPU in the device is not very powerful, and a background process is occupying most of its time. 
+You dump the background process' declarations and instructions to a file (your puzzle input), 
+making sure to use the names of the opcodes rather than the numbers.
+
+For example, suppose you have the following program:
+
+#ip 0
+seti 5 0 1
+seti 6 0 2
+addi 0 1 0
+addr 1 2 3
+setr 1 0 0
+seti 8 0 4
+seti 9 0 5
+
+When executed, the following instructions are executed. Each line contains the value of the instruction pointer at the time the instruction started, the values of the six registers before executing the instructions (in square brackets), the instruction itself, and the values of the six registers after executing the instruction (also in square brackets).
+
+ip=0 [0, 0, 0, 0, 0, 0] seti 5 0 1 [0, 5, 0, 0, 0, 0]
+ip=1 [1, 5, 0, 0, 0, 0] seti 6 0 2 [1, 5, 6, 0, 0, 0]
+ip=2 [2, 5, 6, 0, 0, 0] addi 0 1 0 [3, 5, 6, 0, 0, 0]
+ip=4 [4, 5, 6, 0, 0, 0] setr 1 0 0 [5, 5, 6, 0, 0, 0]
+ip=6 [6, 5, 6, 0, 0, 0] seti 9 0 5 [6, 5, 6, 0, 0, 9]
+In detail, when running this program, the following events occur:
+
+The first line (#ip 0) indicates that the instruction pointer should be bound to register 0 in this program. 
+This is not an instruction, and so the value of the instruction pointer does not change during 
+the processing of this line.
+The instruction pointer contains 0, and so the first instruction is executed (seti 5 0 1). 
+It updates register 0 to the current instruction pointer value (0), sets register 1 to 5, 
+sets the instruction pointer to the value of register 0 (which has no effect, as the instruction 
+did not modify register 0), and then adds one to the instruction pointer.
+The instruction pointer contains 1, and so the second instruction, seti 6 0 2, is executed. 
+This is very similar to the instruction before it: 6 is stored in register 2, and the instruction pointer 
+is left with the value 2.
+The instruction pointer is 2, which points at the instruction addi 0 1 0. 
+This is like a relative jump: the value of the instruction pointer, 2, is loaded into register 0. 
+Then, addi finds the result of adding the value in register 0 and the value 1, storing the result, 3, 
+back in register 0. Register 0 is then copied back to the instruction pointer, 
+which will cause it to end up 1 larger than it would have otherwise and skip the next instruction 
+(addr 1 2 3) entirely. Finally, 1 is added to the instruction pointer.
+The instruction pointer is 4, so the instruction setr 1 0 0 is run. 
+This is like an absolute jump: it copies the value contained in register 1, 5, into register 0, 
+which causes it to end up in the instruction pointer. The instruction pointer is then incremented, 
+leaving it at 6.
+The instruction pointer is 6, so the instruction seti 9 0 5 stores 9 into register 5. 
+The instruction pointer is incremented, causing it to point outside the program, and so the program ends.
+What value is left in register 0 when the background process halts?
+
+===================================================================================
+Notes about this problem.
+
+The opcodes have already been translated into their mnemonic. 
+Could either use the opcode directly and add a "parser" or could translate the opcode into the previous opcode numbers.
+Probably easier to use the opcode directly and parse it.
 
 """
 
@@ -88,12 +175,23 @@ def abbyTerminate(strToPrint):
 ## AFTER[8:11] = The register values after the operation
 
 class CPU:
+	"""The register set is globals to the class
+	"""
 	CPU_Reg0 = 0
 	CPU_Reg1 = 0
 	CPU_Reg2 = 0
 	CPU_Reg3 = 0
+	CPU_Reg4 = 0
+	CPU_Reg5 = 0
+	CPU_IP = 0
 
 	def emulator(self,vector):
+		"""emulator - The function that calls the ALU and returns the return value
+		TBD - extend to increment the CPU Instruction Pointer.
+		
+		:param vector: The instruction vector fields 0-3
+		:returns: the contents of the registers.
+		"""
 		debug_emulator = False
 		if debug_emulator:
 			print 'emulator:',vector
@@ -101,23 +199,47 @@ class CPU:
 		return self.getRegisterAfterValues()
 	
 	def initializeCPU(self):
+		"""Sets the registers in the CPU to zeros.
+		Used at the start of the program to ensure known values in registers.
+		
+		:returns: no return value
+		"""
 		self.CPU_Reg0 = 0
 		self.CPU_Reg1 = 0
 		self.CPU_Reg2 = 0
 		self.CPU_Reg3 = 0
+		self.CPU_Reg4 = 0
+		self.CPU_Reg5 = 0
+		self.CPU_IP = 0
 
 	def setBeforeOperationRegisterValues(self,beforeRegs):
+		"""setBeforeOperationRegisterValues
+		
+		:param beforeRegs: The registers before the operation.
+		:returns: nothing
+		"""
 		#print 'setBeforeOperationRegisterValues: beforeRegs',beforeRegs
 		self.CPU_Reg0 = beforeRegs[0]
 		self.CPU_Reg1 = beforeRegs[1]
 		self.CPU_Reg2 = beforeRegs[2]
 		self.CPU_Reg3 = beforeRegs[3]
+		self.CPU_Reg4 = beforeRegs[4]
+		self.CPU_Reg5 = beforeRegs[5]
 		return
 	
 	def getRegisterAfterValues(self):
-		return [self.CPU_Reg0, self.CPU_Reg1, self.CPU_Reg2, self.CPU_Reg3]
+		"""getRegisterAfterValues - get the contents of the register after the operation completes
+		
+		:returns: CPU registers as a vector (6 elements long)
+		"""
+		return [self.CPU_Reg0, self.CPU_Reg1, self.CPU_Reg2, self.CPU_Reg3, self.CPU_Reg4, self.CPU_Reg5]
 		
 	def getRegA(self,regSelA):
+		"""getRegA - Simulates a 1:6 de-multiplexer
+		
+		:param regSelA: the select for the the A input to the ALU
+		:returns: content of the selected register
+		"""
 		if regSelA == 0:
 			return self.CPU_Reg0
 		elif regSelA == 1:
@@ -126,9 +248,19 @@ class CPU:
 			return self.CPU_Reg2
 		elif regSelA == 3:
 			return self.CPU_Reg3
-		abbyTerminate('getRegA: passed unexpected value')
+		elif regSelA == 4:
+			return self.CPU_Reg4
+		elif regSelA == 5:
+			return self.CPU_Reg5
+		abbyTerminate('getRegA: passed unexpected value for select')
 
 	def getInputA(self,regSelA,immedVsRegFlag):
+		"""getInputA - Implement a 2:1 multiplexer.
+		
+		:param regSelA: The regSelA value itself (immediate value)
+		:param immedVsRegFlag: Flags whether the immediate value or the register value is returned
+		:returns: The output of the 2:1 multiplexer
+		"""
 		if immedVsRegFlag == 'Immediate':
 			return regSelA
 		elif immedVsRegFlag == 'Register':
@@ -136,6 +268,11 @@ class CPU:
 		abbyTerminate('getInputA: needs flag of Immediate or Register')
 
 	def getRegB(self,regSelB):
+		"""getRegB - Simulates a 1:6 de-multiplexer
+		
+		:param regSelB: the select for the the A input to the ALU
+		:returns: content of the selected register
+		"""
 		if regSelB == 0:
 			return self.CPU_Reg0
 		elif regSelB == 1:
@@ -144,9 +281,19 @@ class CPU:
 			return self.CPU_Reg2
 		elif regSelB == 3:
 			return self.CPU_Reg3
+		elif regSelB == 4:
+			return self.CPU_Reg4
+		elif regSelB == 5:
+			return self.CPU_Reg5
 		abbyTerminate('getRegA: passed unexpected value')
 
 	def getInputB(self,regSelB,immedVsRegFlag):
+		"""getInputA - Implement a 2:1 multiplexer.
+		
+		:param regSelA: The regSelA value itself (immediate value)
+		:param immedVsRegFlag: Flags whether the immediate value or the register value is returned
+		:returns: The output of the 2:1 multiplexer
+		"""
 		if immedVsRegFlag == 'Immediate':
 			return regSelB
 		elif immedVsRegFlag == 'Register':
@@ -154,6 +301,13 @@ class CPU:
 		abbyTerminate('getInputB: needs flag of Immediate or Register')
 
 	def storeCVal(self,regSel,cVal):
+		"""storeCVal - Takes the output of the ALU (called C) and routes it to the correct register.
+		Implements a 1:6 demultiplexer into the six registers to load the correct register
+		In hardware this would be one common bus to all register inputs and a LOAD line to clock the correct register
+		
+		:param regSel: Register Select value (0-5) which selects which register gets written.
+		:param: cVal: Output from the ALU (aka "C")
+		"""
 		debug_storeCVal = False
 		if debug_storeCVal:
 			print 'storeCVal: regSel,cVal',regSel,cVal
@@ -165,49 +319,59 @@ class CPU:
 			self.CPU_Reg2 = cVal
 		elif regSel == 3:
 			self.CPU_Reg3 = cVal
+		elif regSel == 4:
+			self.CPU_Reg4 = cVal
+		elif regSel == 5:
+			self.CPU_Reg5 = cVal
 		else:
 			abbyTerminate('storeCVal: passed unexpected value')
 	
 	def doALU(self,opcodeVector):
+		"""doALU - Do the Arithmetic Logic Unit worked
+		Calls the individual routines for each of the opcodes
+		
+		:param opcodeVector: The vector for the instruction.
+		Vector is [opcodeNumber][regs_before][regs_after]
+		"""
 		debug_doALU = False
 		if debug_doALU:
 			print 'doALU: vector',opcodeVector
 			print 'doALU: opcode =',opcodesList[opcodeVector[0]]
-		if opcodeVector[0] == self.addr:
+		if opcodeVector[0] == 'addr':
 			self.INSTR_addr(opcodeVector)
-		elif opcodeVector[0] == self.addi:
+		elif opcodeVector[0] == 'addi':
 			self.INSTR_addi(opcodeVector)
-		elif opcodeVector[0] == self.mulr:
+		elif opcodeVector[0] == 'mulr':
 			self.INSTR_mulr(opcodeVector)
-		elif opcodeVector[0] == self.muli:
+		elif opcodeVector[0] == 'muli':
 			self.INSTR_muli(opcodeVector)
-		elif opcodeVector[0] == self.banr:
+		elif opcodeVector[0] == 'banr':
 			self.INSTR_banr(opcodeVector)
-		elif opcodeVector[0] == self.bani:
+		elif opcodeVector[0] == 'bani':
 			self.INSTR_bani(opcodeVector)
-		elif opcodeVector[0] == self.borr:
+		elif opcodeVector[0] == 'borr':
 			self.INSTR_borr(opcodeVector)
-		elif opcodeVector[0] == self.bori:
+		elif opcodeVector[0] == 'bori':
 			self.INSTR_bori(opcodeVector)
-		elif opcodeVector[0] == self.setr:
+		elif opcodeVector[0] == 'setr':
 			self.INSTR_setr(opcodeVector)
-		elif opcodeVector[0] == self.seti:
+		elif opcodeVector[0] == 'seti':
 			self.INSTR_seti(opcodeVector)
-		elif opcodeVector[0] == self.gtir:
+		elif opcodeVector[0] == 'gtir':
 			self.INSTR_gtir(opcodeVector)
-		elif opcodeVector[0] == self.gtri:
+		elif opcodeVector[0] == 'gtri':
 			self.INSTR_gtri(opcodeVector)
-		elif opcodeVector[0] == self.gtrr:
+		elif opcodeVector[0] == 'gtrr':
 			self.INSTR_gtrr(opcodeVector)
-		elif opcodeVector[0] == self.eqir:
+		elif opcodeVector[0] == 'eqir':
 			self.INSTR_eqir(opcodeVector)
-		elif opcodeVector[0] == self.eqri:
+		elif opcodeVector[0] == 'eqri':
 			self.INSTR_eqri(opcodeVector)
-		elif opcodeVector[0] == self.eqrr:
+		elif opcodeVector[0] == 'eqrr':
 			self.INSTR_eqrr(opcodeVector)
 		else:
 			print 'doALU: opcode =',opcodeVector[0]
-			abbyTerminate('doALU: passed bad opcode')
+			abbyTerminate('doALU: passed bad opcode, exiting...')
 		return
 
 	def INSTR_addr(self,vector):	# (add register) stores into register C the result of adding register A and register B.
@@ -491,6 +655,16 @@ def testRegisters(myCPU):
 	return True
 	
 def testOpcode(myCPU,regsBefore,instructionVector,expectedVector):
+	"""Used to test the ALU executes correctly. 
+	Called from testALU. 
+	
+	:param myCPU: the CPU class
+	:param regsBefore: The registers before the opcode is executed
+	:param instructionVector: The Instruction vector
+	:param expectedVector: The expected contents of the vector after the instruction is executed
+	:returns: True if the ALU operation succeeded (returned the expected value)
+	False if the ALU operation failed
+	"""
 	debug_testOpcode = False
 	if debug_testOpcode:
 		print 'testOpcode: Setting the before operation register values'
@@ -511,6 +685,10 @@ def testOpcode(myCPU,regsBefore,instructionVector,expectedVector):
 	return False
 
 def testALU(myCPU):
+	"""testALU - Run test vectors on the ALU to make sure it operates correctly
+	
+	:param myCPU: The CPU class
+	"""
 	debug_testALU = False
 	if debug_testALU:
 		print 'testALU: Initializing the CPU registers'
