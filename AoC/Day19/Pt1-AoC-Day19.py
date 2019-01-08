8b660b23c86327c27ea418d1317e87940522c819
Doug Gilliland <doug@douglasgilliland.com>
2019-01-08 15:59:03 -0500

    Update Pt1-AoC-Day19.py
    Loading the program into a list.

--- AoC/Day19/Pt1-AoC-Day19.py
+++ AoC/Day19/Pt1-AoC-Day19.py
@@ -1,6 +1,6 @@
 # Pt2-AoCDay19.py
 # 2018 Advent of Code
-# Day 16
+# Day 19
 # Part 1
 # https://adventofcode.com/2018/day/19
 
@@ -103,58 +103,17 @@ def printList(listToPrint):
 	for row in listToPrint:
 		print row
 
-def readTextFileToList(fileName):
-	"""readTextFileAndSrtToList - open file and read the content to a list
+def readtextFileAsListOfLinesToList(fileName):
+	"""readtextFileAsListOfLinesAndSrtToList - open file and read the content to a list
 	File is sorted to produce a date/time ordered file
-	:returns: the list sorted list
+	:returns: the text file as a list of input lines
 	"""
-	textList = []
-	with open(fileName, 'r') as filehandle:  
+	textFileAsListOfLines = []
+	with open(fileName,'r') as filehandle:  
 		for line in filehandle:
-			textList.append(line.strip())
-	#printList(textList)
-	return textList
+			textFileAsListOfLines.append(line.strip())
+	return textFileAsListOfLines
 	
-def parseTextFileIntoListOfNumbers(textFile):
-	"""Convert the text file into a list
-	
-	"""
-	theList = []
-	listOfBefore = []
-	listOfAfter = []
-	opcodeList = []
-	for line in textFile:
-		if len(line) == 0:
-			continue
-		elif line[0:4] == '#ip ':
-			instructionPointer = line[4]
-			print 'instruction pointer',line[4]
-			exit()
-		elif line[0] == 'B':
-			newLine = line[9:-1]
-			newLine = newLine.replace(' ','')
-			listOfBefore = newLine.split(',')
-			#print 'before',
-			#printList(listOfBefore)
-		elif line[0] == 'A':
-			newLine = line[9:-1]
-			newLine = newLine.replace(' ','')
-			listOfAfter = newLine.split(',')
-			#print 'after',listOfAfter
-			theThing = []
-			theThing.extend(opcodeList)
-			theThing.extend(listOfBefore)
-			theThing.extend(listOfAfter)
-			theNewThing = []
-			for item in theThing:
-				theNewThing.append(int(item))
-			theList.append(theNewThing)
-		else:	# opcode case
-			opcodeList = line.split()
-			#print 'opcode',opcodeList
-	#printList(theList)
-	return theList
-
 def abbyTerminate(strToPrint):
 	print 'abbyTerminate: terminating due to',
 	print strToPrint
@@ -188,7 +147,10 @@ class CPU:
 	CPU_Reg4 = 0
 	CPU_Reg5 = 0
 	CPU_IP = 0
-
+	
+	def executeOpCode(self,opCodeToExecute):
+		return True
+		
 	def emulator(self,vector):
 		"""emulator - The function that calls the ALU and returns the return value
 		TBD - extend to increment the CPU Instruction Pointer.
@@ -502,276 +464,61 @@ class CPU:
 			cVal = 0
 		self.storeCVal(vector[3],cVal)
 	
-	#opcodes that failed
-	eqri = 0	# 15
-	bori = 1	# could be 2,4,7,10,11,13,15
-	mulr = 2	# could be 2, not 14
-	seti = 3	# could be 3,9
-	banr = 4	# could be 2,4,5,7,9,10,11,13,15
-	bani = 5	# could be 2,4,5,7,10,11,13,15
-	borr = 6	# could be 0,1,2,4,4,5,6,7,8,9,10,12,14
-	gtrr = 7	# could be 7,15
-	gtir = 8	# could be 10
-	addi = 9	# could be 2,4,5,9,10,11,14,15
-	setr = 10	# could be 8,10,
-	eqrr = 11	# could be 11
-	addr = 12	# could be 12
-	eqir = 13	# could be 13
-	gtri = 14	# could be 1,2,3,4,6,8,9,11,13,14
-	muli = 15	# 15
-	
 #########################################################################
 ## This is the workhorse of this assignment
-## Test opcodes one at a time against all of the instructions
-## Can the instructions be sorted/grouped or are they dependent on previous values in the registers?
-## Registers start at value 0
-## Should registers be global variable or accessed via functions?
-"""	addr = 0
-	addi = 1
-	mulr = 2
-	muli = 3
-	banr = 4
-	bani = 5
-	borr = 6
-	bori = 7
-	setr = 8
-	seti = 9
-	gtir = 10
-	gtri = 11
-	gtrr = 12
-	eqir = 13
-	eqri = 14
-	eqrr = 15
-"""
 
 opcodesList = ['eqri','bori','mulr','seti','banr','bani','borr','gtrr','gtir','addi','setr','eqrr','addr','eqir','gtri','muli',]
-	
-def processList(theList,myCPU):
-	"""
-	[[0, 0], 
-	[1, 0, 4, 7, 8, 11, 13, 14], 
-	[2, 1, 2, 3, 4, 6, 9, 10, 11, 13, 14], 
-	[3, 0, 2, 3, 4, 5, 8, 11, 14], 
-	[4, 0, 3, 4, 5, 7, 8, 11, 13, 14], 
-	[5, 0, 4, 5, 7, 8, 11, 13, 14], 
-	[6, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15], 
-	[7, 0, 7], 
-	[8, 8, 10], 
-	[9, 3, 9], 
-	[10, 0, 7, 8, 11, 13, 14], 
-	[11, 2, 3, 4, 5, 6, 10, 11, 13], 
-	[12, 3, 9, 12], [13, 0, 11, 13, 14], 
-	[14, 7, 14], 
-	[15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]]
-	"""
-	listByOpcode = sorted(theList, key = lambda errs: errs[0])
-	listVals = []
-	for opCodeVal in xrange(16):
-		listLine = []
-		listLine.append(opCodeVal)
-		passingCases = 0
-		failedCases = 0
-		passBins = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0}
-		failBins = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0}
-		for testVec in listByOpcode:
-			if testVec[0] == opCodeVal:
-				for myOpCodes in range(16):
-					myCPU.initializeCPU()
-					testVec[0] = myOpCodes
-					if testOpcode(myCPU,testVec[4:8],testVec[0:4],testVec[8:12]):
-						passBins[testVec[0]] = passBins[testVec[0]] + 1
-						passingCases += 1
-					else:
-						failBins[testVec[0]] = failBins[testVec[0]] + 1
-						failedCases += 1
-		print opCodeVal,',',
-		print opcodesList[opCodeVal],',',
-		for x,y in passBins.items():
-			if y > 0:
-				print x,',',
-				listLine.append(x)
-		print
-		listVals.append(listLine)
-		
-	print listVals
-	print 'passBins',passBins
-	print 'failedCases',failedCases
-	print 'failBins',failBins
-	
-def testRegisters(myCPU):
-	"""tests the register write path via the store of the ALU output - storeCVal
-	tests the readback of the register values via getRegisterAfterValues
-	"""
-	debug_testRegisters = False
-	afterRegs = myCPU.getRegisterAfterValues()
-	if afterRegs == [0,0,0,0]:
-		if debug_testRegisters:
-			print 'testRegisters: CPU registers were intialized to zeros'
-	else:
-		abbyTerminate('testRegisters: CPU registers did not initialize')
-	if debug_testRegisters:
-		print 'testRegisters: storing 12 to register 0'
-	myCPU.storeCVal(0,12)
-	afterRegs = myCPU.getRegisterAfterValues()
-	if debug_testRegisters:
-		print 'testRegisters: afterRegs',afterRegs
-	if afterRegs == [12,0,0,0]:	
-		if debug_testRegisters:
-			print 'testRegisters: Register 0 was set to 12'
-	else:
-		abbyTerminate('testRegisters: CPU registers did not initialize')
-	myCPU.storeCVal(1,15)
-	afterRegs = myCPU.getRegisterAfterValues()
-	if debug_testRegisters:
-		print 'testRegisters: afterRegs',afterRegs
-	if afterRegs == [12,15,0,0]:	
-		if debug_testRegisters:
-			print 'testRegisters: Register 1 was set to 15'
-	else:
-		abbyTerminate('testRegisters: CPU registers did not initialize')
-	myCPU.storeCVal(2,18)
-	afterRegs = myCPU.getRegisterAfterValues()
-	if debug_testRegisters:
-		print 'testRegisters: afterRegs',afterRegs
-	if afterRegs == [12,15,18,0]:	
-		if debug_testRegisters:
-			print 'testRegisters: Register 2 was set to 18'
-	else:
-		abbyTerminate('testRegisters: CPU registers did not initialize')
-	myCPU.storeCVal(3,21)
-	afterRegs = myCPU.getRegisterAfterValues()
-	if debug_testRegisters:
-		print 'testRegisters: afterRegs',afterRegs
-	if afterRegs == [12,15,18,21]:
-		if debug_testRegisters:
-			print 'testRegisters: Register 3 was set to 21'
-	else:
-		abbyTerminate('testRegisters: CPU registers did not initialize')
-	if debug_testRegisters:
-		print 'testRegisters: register tests passed'
-	myCPU.initializeCPU()	# reset registers
-	afterRegs = myCPU.getRegisterAfterValues()
-	if afterRegs == [0,0,0,0]:
-		if debug_testRegisters:
-			print 'testRegisters: initializeCPU function passed'
-	else:
-		abbyTerminate('testRegisters: initializeCPU function failed to clear CPU registers')
-	return True
-	
-def testOpcode(myCPU,regsBefore,instructionVector,expectedVector):
-	"""Used to test the ALU executes correctly. 
-	Called from testALU. 
-	
-	:param myCPU: the CPU class
-	:param regsBefore: The registers before the opcode is executed
-	:param instructionVector: The Instruction vector
-	:param expectedVector: The expected contents of the vector after the instruction is executed
-	:returns: True if the ALU operation succeeded (returned the expected value)
-	False if the ALU operation failed
-	"""
-	debug_testOpcode = False
-	if debug_testOpcode:
-		print 'testOpcode: Setting the before operation register values'
-	myCPU.setBeforeOperationRegisterValues(regsBefore)	# Using problem example
-	if debug_testOpcode:
-		print 'testOpcode: Setting the instruction vector'
-		print 'testOpcode: calling the emulator code'
-	myCPU.emulator(instructionVector)
-	afterRegs = myCPU.getRegisterAfterValues()
-	if debug_testOpcode:
-		print 'testOpcode:afterRegs',afterRegs
-	if afterRegs == expectedVector:
-		if debug_testOpcode:
-			print 'testOpcode: opcode',instructionVector[0],' passed'
-		return True
-	if debug_testOpcode:
-		print 'testOpcode: opcode',instructionVector[0],' failed'
-	return False
 
-def testALU(myCPU):
-	"""testALU - Run test vectors on the ALU to make sure it operates correctly
+def convertOpcodeStringToVector(opcodeString):
+	"""convertOpcodeStringToVector - Convert the string from the file into a list
+	Two types of strings
+	Instruction Pointer updates
+	Opcode plus operands.
 	
-	:param myCPU: The CPU class
+	:param opcodeString: Example - addi 1 16 1
+	:returns: list of the instruction with the operand strings converted to ints
 	"""
-	debug_testALU = False
-	if debug_testALU:
-		print 'testALU: Initializing the CPU registers'
-	myCPU.initializeCPU()	# reset registers at the end of the testALU
-	if not testOpcode(myCPU,[3,2,1,1],[0,2,1,2],[3,2,3,1]):		# addr
-		abbyTerminate('opcode 0 failed')
-	if not testOpcode(myCPU,[3,2,1,1],[1,2,1,2],[3,2,2,1]):		# addi
-		abbyTerminate('opcode 1 failed')
-	if not testOpcode(myCPU,[3,2,1,1],[2,2,1,2],[3,2,2,1]):		# mulr
-		abbyTerminate('opcode 2 failed')
-	if not testOpcode(myCPU,[3,4,5,6],[3,2,1,3],[3,4,5,5]):		# muli
-		abbyTerminate('opcode 3 failed')
-	if not testOpcode(myCPU,[3,2,1,1],[9,2,1,2],[3,2,2,1]):		# seti
-		abbyTerminate('opcode 9 failed')
-	if testOpcode(myCPU,[3,2,1,1],[4,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 4 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[5,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 5 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[6,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 6 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[7,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 7 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[8,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 8 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[10,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 10 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[11,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 11 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[12,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 12 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[13,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 13 should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[14,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 14  should not have passed')
-	if testOpcode(myCPU,[3,2,1,1],[15,2,1,2],[3,2,2,1]):			# 
-		abbyTerminate('opcode 15 should not have passed')
-	return True
-	
-def testCPU(myCPU):
-	debug_testCPU = False
-	if debug_testCPU:
-		print 'testCPU: Initialized CPU Class'
-	myCPU.initializeCPU()
-	
-	if testRegisters(myCPU):
-		if debug_testCPU:
-			print 'testCPU: testRegisters passed'
-	
-	if testALU(myCPU):
-		if debug_testCPU:
-			print 'testCPU: ALU tests passed'	
-	return True
-	
-def processListPart1(theList,myCPU):
-	listByOpcode = sorted(theList, key = lambda errs: errs[0])
-	totalThatHaveThreeOrMoreOpcodes = 0
-	for testVec in listByOpcode:
-		testCaseMatches = 0
-		for x in range(16):
-			testVecReplacement = testVec[0:4]
-			testVecReplacement[0] = x
-			if testOpcode(myCPU,testVec[4:8],testVecReplacement,testVec[8:12]):
-				testCaseMatches += 1
-		if testCaseMatches >= 3:
-			totalThatHaveThreeOrMoreOpcodes += 1
-	print 'totalThatHaveThreeOrMoreOpcodes',totalThatHaveThreeOrMoreOpcodes
+	debug_convertOpcodeStringToVector = False
+	opcodeList = opcodeString.split()
+	opcodeVector = [opcodeList[0],int(opcodeList[1]),int(opcodeList[2]),int(opcodeList[3])]
+	if debug_convertOpcodeStringToVector:
+		print 'convertOpcodeStringToVector: opcodeVector',opcodeVector
+	if opcodeVector[0] not in opcodesList:
+		abbyTerminate('convertOpcodeStringToVector: opcode not in the opcode list, exiting')
+	return opcodeVector
 
+instructionPointer = 0
+instructionPointerRegisterNumber = 0
+
+def loadProgramToList(textFileAsListOfLines,myCPU):
+	"""Convert the text file into a list
+	
+	:param textFileAsListOfLines: the input file as a list of strings where each string is one line of the input file
+	:param myCPU: - points to the CPU class
+	"""
+	programListing = []
+	for line in textFileAsListOfLines:
+		if len(line) == 0:				# skip blank lines
+			continue
+		elif line[0:4] == '#ip ':		# Bind the Instruction Counter to a particular register
+			instructionPointerRegisterNumber = int(line[4])
+		else:							# opcode case
+			newOpCode = convertOpcodeStringToVector(line)
+			programListing.append(newOpCode)
+	print 'loadProgramToList: program is'
+	for location in programListing:
+		print location
+	return programListing
 
 ########################################################################
 ## Code
 
 print 'Reading in file',time.strftime('%X %x %Z')
 
-textList = readTextFileToList('input.txt')
-
-myList = parseTextFileIntoListOfNumbers(textList)
+textList = readtextFileAsListOfLinesToList('input.txt')
 
 myCPU = CPU()
 
-processListPart1(myList,myCPU)
+loadProgramToList(textList,myCPU)
 
 print 'Completed processing',time.strftime('%X %x %Z')
