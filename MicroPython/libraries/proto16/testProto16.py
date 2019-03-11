34d2f8c44db4bdc01cc4cfa1386c0f0c3a0a3509
Doug Gilliland <doug@douglasgilliland.com>
2019-03-11 10:13:24 -0400

    Untested driver - WIP

--- MicroPython/libraries/proto16/testProto16.py
+++ MicroPython/libraries/proto16/testProto16.py
@@ -3,12 +3,12 @@
 ###############################################################################
 
 import time
-import mcp23017
+import mcp23017		# Land Board library which uses Arduino style functions
 import sys
 
 def testProto16():
 	start = time.ticks_ms()
-	print("Bounce LED on mcp23017-I2C card")
+	print("Bounce LED on PROTO16-I2C card")
 
 	# Set all of the pins to pulled up inputs
 	for bit in range(0,16):
@@ -18,7 +18,7 @@ def testProto16():
 			print("testProto16 (1): readback failed - expected 0, got 1")
 			sys.exit(1)
 		if mcp23017.digitalRead(bit)!=0:
-			print("testProto16 (1): readback failed - expected 0, got 1")
+			print("testProto16 (2): readback failed - expected 0, got 1")
 			sys.exit(1)
 		
 	# Blink all LEDs
@@ -27,7 +27,7 @@ def testProto16():
 			mcp23017.digitalWrite(bit,1)
 			time.sleep(0.5)
 			if mcp23017.digitalRead(bit)!=1:
-				print("testProto16 (2): readback failed - expected 1, got 0")
+				print("testProto16 (3): readback failed - expected 1, got 0")
 				sys.exit(1)
 			mcp23017.digitalWrite(bit,0)
 	deltaTime = time.ticks_diff(start, time.ticks_ms())/1000
