4562a6ecbadb0cb077398f24ced64a9b182c1384
Doug Gilliland <doug@douglasgilliland.com>
2019-03-11 10:15:55 -0400

    Set speed, set chip base address

--- MicroPython/libraries/blinkMcp23017Led.py
+++ MicroPython/libraries/blinkMcp23017Led.py
@@ -9,17 +9,19 @@
 import time
 import machine
 
-i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(21))		# Set up pins
+MCP23017_BASEADDR=0x20
+
+i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(21),freq=200000)		# Set up pins
 outdata=bytearray(b'\xfe') 				# IODIRA value lowest bit output
-i2c.writeto_mem(0x20,0x00,outdata) 		# Set the IODIRA to output for lowest bit
+i2c.writeto_mem(MCP23017_BASEADDR,0x00,outdata) 		# Set the IODIRA to output for lowest bit
 
 def blinkLED0():
 	for loopCount in range(0,10):
 		outdata=bytearray(b'\x01') 			# Set LED high
-		i2c.writeto_mem(0x20,0x14,outdata)	# Write to OLATA register
+		i2c.writeto_mem(MCP23017_BASEADDR,0x14,outdata)	# Write to OLATA register
 		time.sleep(0.25)
 		outdata=bytearray(b'\x00')			# Set LED low
-		i2c.writeto_mem(0x20,0x14,outdata)	# Write to OLATA register
+		i2c.writeto_mem(MCP23017_BASEADDR,0x14,outdata)	# Write to OLATA register
 		time.sleep(0.25)
 
 blinkLED0()
