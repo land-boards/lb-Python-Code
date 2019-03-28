a7c76f3f9a7ce09874b3da3e93da0a9f6a15f01d
Doug Gilliland <doug@douglasgilliland.com>
2019-03-28 10:49:38 -0400

    Update I2C_EEPROM.py

--- MicroPython/PyBoard Clone/libraries/I2C_EEPROM/I2C_EEPROM.py
+++ MicroPython/PyBoard Clone/libraries/I2C_EEPROM/I2C_EEPROM.py
@@ -14,26 +14,70 @@ EEPROMAddress = 0x50
 
 i2c=machine.I2C(1)		# Set up pins
 
-def readEEPROM(memAdr):
+def readEEPROM_8bits(memAdr):
+	"""readEEPROM_8bits - read a single byte from the EEPROM
+	
+	:param memAdr: EEPROM 8-bit memory address (0-0xff)
+	:returns: Character that was read from the EEPROM
+	"""
 	readbackVal=bytearray(1)
 	i2c.readfrom_mem_into(boardBaseAddress + EEPROMAddress,memAdr,readbackVal)
 	rwValue=readbackVal[0]
 	return rwValue
 	
-def writeEEPROM(memAdr,wrValue):
+def writeEEPROM_8bits(memAdr,wrValue):
+	"""writeEEPROM_8bits
+	
+	:param memAdr: EEPROM 8-bit memory address (0-0xff)
+	:param wrValue: Write value
+	:returns: no return value
+	"""
 	outBuff=bytearray(1)
 	outBuff[0]=wrValue
 	i2c.writeto_mem(0x20,memAdr,outBuff)
 	return
 
+def readEEPROM_String(memAdr,length):
+	"""readEEPROM_String - Read in a string from the EEPROM
+	Reads in the number of bytes or stops when a null (0x0) is reached
+	
+	:param memAdr: EEPROM 8-bit memory address (0-0xff)
+	:param length: Number of bytes to read
+	:returns: String that was read from the EEPROM - null terminated
+	"""
+	retString = ''
+	lenCounter = length
+	adrEEPROM = memAdr
+	for x in range(length):
+		retChar = readEEPROM_8bits(adrEEPROM)
+		retString += retChar
+		if retChar == 0x0:
+			return retString
+	return retString
+	
+def writeEEPROM_String(memAdr,stringToWrite):
+	"""writeEEPROM_String
+	
+	:param memAdr: EEPROM 8-bit memory address (0-0xff)
+	:param stringToWrite: The string that is being written to EEPROM
+	:returns: no return value
+	"""
+	adrEEPROM = memAdr
+	for charToWrite in stringToWrite:
+		writeEEPROM_8bits(adrEEPROM,charToWrite)
+		adrEEPROM += 1
+
 def testEEPROM():
-	writeEEPROM(0,0xde)
-	writeEEPROM(1,0xad)
-	rdVal = readEEPROM(0)
+	"""testEEPROM - Test the EEPROM
+	Check a couple of locations to make sure they can be written and read back
+	"""
+	writeEEPROM_8bits(0,0xde)
+	writeEEPROM_8bits(1,0xad)
+	rdVal = readEEPROM_8bits(0)
 	if rdVal != 0xde:
 		print("testEEPROM(1): Write 0xde, read back", rdVal)
 		exit()
-	rdVal = readEEPROM(1)
+	rdVal = readEEPROM_8bits(1)
 	if rdVal != 0xad:
 		print("testEEPROM(2): Write 0xad, read back", rdVal)
 		exit()
