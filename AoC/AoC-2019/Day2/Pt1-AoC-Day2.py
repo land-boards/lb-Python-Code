8de0abc47a80064100a4690ff4afb0842b920d57
Doug Gilliland <doug@douglasgilliland.com>
2019-12-02 09:50:28 -0500

    Day 2

--- AoC/AoC-2019/Day2/Pt1-AoC-Day2.py
+++ AoC/AoC-2019/Day2/Pt1-AoC-Day2.py
@@ -3,37 +3,91 @@
 # Day 1
 # Part 1
 """
---- Day 1: The Tyranny of the Rocket Equation ---
-Santa has become stranded at the edge of the Solar System while delivering presents to other planets! To accurately calculate his position in space, safely align his warp drive, and return to Earth in time to save Christmas, he needs you to bring him measurements from fifty stars.
+--- Day 2: 1202 Program Alarm ---
 
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+On the way to your gravity assist around the Moon, your ship computer beeps angrily about a "1202 program alarm". On the radio, an Elf is already explaining how to handle the situation: "Don't worry, that's perfectly norma--" The ship computer bursts into flames.
 
-The Elves quickly load you into a spacecraft and prepare to launch.
+You notify the Elves that the computer's magic smoke seems to have escaped. "That computer ran Intcode programs like the gravity assist program it was working on; surely there are enough spare parts up there to build a new Intcode computer!"
 
-At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper. They haven't determined the amount of fuel required yet.
+An Intcode program is a list of integers separated by commas (like 1,0,0,3,99). To run one, start by looking at the first integer (called position 0). Here, you will find an opcode - either 1, 2, or 99. The opcode indicates what to do; for example, 99 means that the program is finished and should immediately halt. Encountering an unknown opcode means something went wrong.
 
-Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+Opcode 1 adds together numbers read from two positions and stores the result in a third position. The three integers immediately after the opcode tell you these three positions - the first two indicate the positions from which you should read the input values, and the third indicates the position at which the output should be stored.
 
-For example:
+For example, if your Intcode computer encounters 1,10,20,30, it should read the values at positions 10 and 20, add those values, and then overwrite the value at position 30 with their sum.
 
-For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
-For a mass of 1969, the fuel required is 654.
-For a mass of 100756, the fuel required is 33583.
-The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
+Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them. Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
 
-What is the sum of the fuel requirements for all of the modules on your spacecraft?
-Your puzzle answer was 3364329.
+Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
+
+For example, suppose you have the following program:
+
+1,9,10,3,2,3,11,0,99,30,40,50
+
+For the purposes of illustration, here is the same program split into multiple lines:
+
+1,9,10,3,
+2,3,11,0,
+99,
+30,40,50
+
+The first four integers, 1,9,10,3, are at positions 0, 1, 2, and 3. Together, they represent the first opcode (1, addition), the positions of the two inputs (9 and 10), and the position of the output (3). To handle this opcode, you first need to get the values at the input positions: position 9 contains 30, and position 10 contains 40. Add these numbers together to get 70. Then, store this value at the output position; here, the output position (3) is at position 3, so it overwrites itself. Afterward, the program looks like this:
+
+1,9,10,70,
+2,3,11,0,
+99,
+30,40,50
+
+Step forward 4 positions to reach the next opcode, 2. This opcode works just like the previous, but it multiplies instead of adding. The inputs are at positions 3 and 11; these positions contain 70 and 50 respectively. Multiplying these produces 3500; this is stored at position 0:
+
+3500,9,10,70,
+2,3,11,0,
+99,
+30,40,50
+
+Stepping forward 4 more positions arrives at opcode 99, halting the program.
+
+Here are the initial and final states of a few more small programs:
+
+    1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
+    2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
+    2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
+    1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
+
+Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
+
 """
 from __future__ import print_function
 
-accum = 0	# Accumulated sum
+def processList(listOfNumbers):
+	print("Length of list is :",len(listOfNumbers))
+	opCodeOffset = 0
+	while 1:
+		if listOfNumbers[opCodeOffset] == 99:
+			print("end")
+			print("list of numbers : ",listOfNumbers)
+			exit()
+		elif listOfNumbers[opCodeOffset] == 1:
+			pos1 = listOfNumbers[opCodeOffset+1]
+			pos2 = listOfNumbers[opCodeOffset+2]
+			posOut = listOfNumbers[opCodeOffset+3]
+			listOfNumbers[posOut] = listOfNumbers[pos1] + listOfNumbers[pos2]
+			print("Add result :",listOfNumbers[posOut])
+			opCodeOffset = opCodeOffset + 4
+		elif listOfNumbers[opCodeOffset] == 2:
+			pos1 = listOfNumbers[opCodeOffset+1]
+			pos2 = listOfNumbers[opCodeOffset+2]
+			posOut = listOfNumbers[opCodeOffset+3]
+			listOfNumbers[posOut] = listOfNumbers[pos1] * listOfNumbers[pos2]
+			print("Multiply result :",listOfNumbers[posOut])
+			opCodeOffset = opCodeOffset + 4
+		else:
+			print("error")
 
 # open file and read the content into an accumulated sum
 with open('input.txt', 'r') as filehandle:  
-	for lineIn in filehandle:
-		currentPlace = int(int(lineIn.strip()) / 3)-2
-		print (lineIn)
-		print (currentPlace)
-		accum += currentPlace
-print('sum =',accum)
+	inLine = filehandle.readline()
+	numbers = map(int, inLine.split(','))
+	numbers[1] = 82
+	numbers[2]= 50
+	print(numbers)
+processList(numbers)
---
+++ AoC/AoC-2019/Day2/Pt2-AoC-Day2.py
@@ -0,0 +1,90 @@
+# Pt1-AoCDay1.py
+# 2018 Advent of Code
+# Day 1
+# Part 1
+"""
+--- Part Two ---
+
+"Good, the new computer seems to be working correctly! Keep it nearby during this mission - you'll probably use it again. Real Intcode computers support many more features than your new one, but we'll let you know what they are as you need them."
+
+"However, your current priority should be to complete your gravity assist around the Moon. For this mission to succeed, we should settle on some terminology for the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory. When you run an Intcode program, make sure to start by initializing memory to the program's values. A position in memory is called an address (for example, the first value in memory is at "address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used immediately after an opcode, if any, are called the instruction's parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters. The instruction 99 contains only an opcode and has no parameters.
+
+The address of the current instruction is called the instruction pointer; it starts at 0. After an instruction finishes, the instruction pointer increases by the number of values in the instruction; until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions. (The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed. To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+
+The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before. In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb. Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just like before. Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program (your puzzle input) - in other words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+
+
+"""
+from __future__ import print_function
+
+def processList(listOfNumbers):
+#	print("Length of list is :",len(listOfNumbers))
+	opCodeOffset = 0
+	while 1:
+		if listOfNumbers[opCodeOffset] == 99:
+#			print("end")
+#			print("list of numbers : ",listOfNumbers)
+			return(listOfNumbers[0])
+		elif listOfNumbers[opCodeOffset] == 1:
+			pos1 = listOfNumbers[opCodeOffset+1]
+			pos2 = listOfNumbers[opCodeOffset+2]
+			posOut = listOfNumbers[opCodeOffset+3]
+			listOfNumbers[posOut] = listOfNumbers[pos1] + listOfNumbers[pos2]
+#			print("Add result :",listOfNumbers[posOut])
+			opCodeOffset = opCodeOffset + 4
+		elif listOfNumbers[opCodeOffset] == 2:
+			pos1 = listOfNumbers[opCodeOffset+1]
+			pos2 = listOfNumbers[opCodeOffset+2]
+			posOut = listOfNumbers[opCodeOffset+3]
+			listOfNumbers[posOut] = listOfNumbers[pos1] * listOfNumbers[pos2]
+#			print("Multiply result :",listOfNumbers[posOut])
+			opCodeOffset = opCodeOffset + 4
+		else:
+			print("error")
+
+# open file and read the content into an accumulated sum
+noun = 1
+verb = 1
+result = 0
+numElements = 0
+with open('input.txt', 'r') as filehandle:
+	inLine = filehandle.readline()
+	numbers = map(int, inLine.split(','))
+	numElements = len(numbers)
+	print("Element Count :",numElements)
+	numElements = numElements - 1
+while (result <= 19690720):
+	noun = 1
+	while (noun < numElements):
+		verb = 1
+		while(verb < numElements):
+			with open('input.txt', 'r') as filehandle:
+				inLine = filehandle.readline()
+				numbers = map(int, inLine.split(','))
+				numbers[1] = noun
+				numbers[2]= verb
+#				print(numbers)
+			result = processList(numbers)
+			if (result == 19690720):
+				print("result :",result)
+				print("Noun :",noun)
+				print("Verb :",verb)
+				product = (100 * noun) + verb
+				print("Product :",product)
+				exit()
+			verb = verb + 1
+		noun = noun + 1
+print("Noun :",noun)
+print("Verb :",verb)
+product = (100 * noun) + verb
+print("Product :",product)
