4eab669cfda666ba419ea851d75dbbdee22b59f0
Doug Gilliland <doug@douglasgilliland.com>
2019-12-24 08:29:45 -0500

    Test updated IntCode

--- AoC/AoC-2019/Day15 (IntCode Repair Droid)/Pt2-AoC-Day15.py
+++ AoC/AoC-2019/Day15 (IntCode Repair Droid)/Pt2-AoC-Day15.py
@@ -64,23 +64,46 @@ class CPU:
 	Takes input value
 	Returns the output value
 	"""
-	progState = ''
-	programCounter = 0
-	relativeBaseRegister = 0
 	
+	def __init__(self):
+		# self.inputQueue = []
+		# self.outputQueue = []
+		# self.progState = ''
+		# self.programCounter = 0
+		# self.relativeBaseRegister = 0
+		self.programMemory = []
+
+		debug_initCPU = False
+		# state transitions are 
+		# 'inputReady' => 'waitingOnInput' => 
+		# 'inputReady' => 'waitingOnInput' => 
+		# 'progDone'
+		self.setProgState('initCPU')
+		self.programCounter = 0
+		self.relativeBaseRegister = 0
+		self.inputQueue = []
+		self.outputQueue = []
+		self.loadIntCodeProgram()
+		if debug_initCPU:
+			print("Memory Dump :",self.programMemory)
+		
 	def getProgState(self):
-		#print("getProgState: progState =",self.progState)
+		""" Returns the value of the program state variable
+		"""
+		#print("getProgState: self.progState =",self.progState)
 		return self.progState
 	
 	def setProgState(self,state):
+		""" Sets the value of the program state variable
+		"""
 		debug_setProgState = False
 		self.progState = state
 		if debug_setProgState:
-			print("setProgState: progState =",self.progState)
+			print("setProgState: self.progState =",self.progState)
 			
 	def intTo5DigitString(self, instruction):
-		"""Takes a variable length string and packs the front with zeros to make it
-		5 digits long.
+		"""Takes a variable length string and packs the front with zeros 
+		to make it 5 digits long.
 		"""
 		instrString=str(instruction)
 		if len(instrString) == 1:
@@ -96,11 +119,11 @@ class CPU:
 
 	def extractFieldsFromInstruction(self, instruction):
 		""" Take the Instruction and turn into opcode fields
-		ABCDE
+		ABCD
 		A = mode of 3rd parm
 		B = mode of 2nd parm
 		C = mode of 1st parm
-		DE = opcode
+		D = opcode
 		
 		:returns: [opcode,parm1,parm2,parm3]
 		"""
@@ -112,21 +135,11 @@ class CPU:
 		retVal=[opcode,parm1,parm2,parm3]
 		return retVal
 
-	def initCPU(self):
-		debug_initCPU = False
-		# state transitions are 
-		# 'inputReady' => 'waitingOnInput' => 
-		# 'inputReady' => 'waitingOnInput' => 
-		# 'progDone'
-		self.setProgState('initCPU')
-		self.programCounter = 0
-		self.relativeBaseRegister = 0
-		if debug_initCPU:
-			print("Memory Dump :",programMemory)
-		
 	def evalOpPair(self, currentOp):
+		""" Evaluages the two values for instruction like ADD, MUL
+		Returns the two values as a list pair
+		"""
 		debug_BranchEval = False
-		global programMemory
 		if debug_BranchEval:
 			print("         evalOpPair: currentOp =",currentOp)
 		val1 = self.dealWithOp(currentOp,1)
@@ -134,73 +147,87 @@ class CPU:
 		return[val1,val2]
 	
 	def dealWithOp(self,currentOp,offset):
+		""" Single place to interpret opcodes which read program memory
+		Input the opcode field and the offset to the correct opcode field
+		"""
+		#global self.programMemory
+		#global self.programCounter
 		debug_dealWithOp = False
 		if currentOp[offset] == 0:	# position mode
-			val1 = programMemory[programMemory[self.programCounter+offset]]
+			val = self.programMemory[self.programMemory[self.programCounter+offset]]
 			if debug_dealWithOp:
-				print("         dealWithOp: Position Mode Parm",offset,"pos :",self.programCounter+offset,"value =",val1)
+				print("         dealWithOp: Position Mode Parm",offset,"pos :",self.programCounter+offset,"value =",val)
 		elif currentOp[offset] == 1:	# immediate mode
-			val1 = programMemory[self.programCounter+offset]
+			val = self.programMemory[self.programCounter+offset]
 			if debug_dealWithOp:
-				print("         dealWithOp: Immediate Mode parm",offset,": value =",val1)
+				print("         dealWithOp: Immediate Mode parm",offset,": value =",val)
 		elif currentOp[offset] == 2:	# relative mode
-			val1 = programMemory[programMemory[self.programCounter+offset] + self.relativeBaseRegister]
+			val = self.programMemory[self.programMemory[self.programCounter+offset] + self.relativeBaseRegister]
 			if debug_dealWithOp:
-				print("         dealWithOp: Relative Mode parm",offset,": value =",val1)
+				print("         dealWithOp: Relative Mode parm",offset,": value =",val)
 		else:
 			assert False,"dealWithOp: WTF-dealWithOp"
-		return val1
+		return val
 	
-	def writeOp3Result(self,opcode,result):
-		global programMemory
-		global programCounter
+	def writeOpResult(self,opcode,opOffset,val):
+		#global self.programMemory
+		#global self.programCounter
 		debug_writeEqLtResult = False
-		if opcode[3] == 0:
-			programMemory[programMemory[self.programCounter+3]] = result
+		if opcode[opOffset] == 0:
+			self.programMemory[self.programMemory[self.programCounter+opOffset]] = val
 			if debug_writeEqLtResult:
-				print("         output position mode comparison result =",result)
-		elif opcode[3] == 1:
-			programMemory[self.programCounter+3] = result
+				print("         output position mode comparison val =",val)
+		elif opcode[opOffset] == 1:
+			self.programMemory[self.programCounter+opOffset] = val
 			if debug_writeEqLtResult:
-				print("         output immediate mode comparison result =",result,)
-		elif opcode[3] == 2:
-			programMemory[programMemory[self.programCounter+3] + self.relativeBaseRegister] = result
+				print("         output immediate mode comparison val =",val,)
+		elif opcode[opOffset] == 2:
+			self.programMemory[self.programMemory[self.programCounter+opOffset] + self.relativeBaseRegister] = val
 			if debug_writeEqLtResult:
-				print("         output relative mode comparison result =",result,)
+				print("         output relative mode comparison val =",val,)
 	
 	def runCPU(self):
+#		debug_runCPU = True
 		debug_runCPU = False
-		global inputQueue
-		global outputQueue
+		#global self.programMemory
+		#global self.inputQueue
+		#global self.outputQueue
 		while(1):
-			currentOp = self.extractFieldsFromInstruction(programMemory[self.programCounter])
+			currentOp = self.extractFieldsFromInstruction(self.programMemory[self.programCounter])
 			#self.getProgState()
 			if currentOp[0] == 1:		# Addition Operator
 				if debug_runCPU:
-					print("PC =",self.programCounter,"ADD")
+					print("PC =",self.programCounter,"ADD Opcode = ",currentOp," ",end='')
 				result = self.dealWithOp(currentOp,1) + self.dealWithOp(currentOp,2)
-				self.writeOp3Result(currentOp,result)
+				if debug_runCPU:
+					print(self.dealWithOp(currentOp,1),"+",self.dealWithOp(currentOp,2),"=",result)
+				self.writeOpResult(currentOp,3,result)
 				self.programCounter = self.programCounter + 4
 			elif currentOp[0] == 2:		# Multiplication Operator
 				if debug_runCPU:
-					print("PC =",self.programCounter,"MUL")
+					print("PC =",self.programCounter,"MUL Opcode = ",currentOp," ",end='')
 				result = self.dealWithOp(currentOp,1) * self.dealWithOp(currentOp,2)
-				self.writeOp3Result(currentOp,result)
+				if debug_runCPU:
+					print(self.dealWithOp(currentOp,1),"*",self.dealWithOp(currentOp,2),"=",result)
+				self.writeOpResult(currentOp,3,result)
 				self.programCounter = self.programCounter + 4
 			elif currentOp[0] == 3:		# Input Operator
 				debug_CPUInput = False
 #				debug_CPUInput = True
 				if debug_runCPU or debug_CPUInput:
 					print("PC =",self.programCounter,"INP Opcode = ",currentOp,end='')
-				if len(inputQueue) == 0:
+				if len(self.inputQueue) == 0:
 					if debug_runCPU or debug_CPUInput:
 						print(" - Returning to main for input value")
 					self.setProgState('waitForInput')
 					return
-				programMemory[programMemory[self.programCounter+1]] = inputQueue[0]
 				if debug_runCPU or debug_CPUInput:
-					print(" value =",inputQueue[0])
-				del inputQueue[0]
+					print(" value =",self.inputQueue[0])
+				result = self.inputQueue[0]
+				self.writeOpResult(currentOp,1,result)
+				del self.inputQueue[0]	 # Empty the input queue
+				# if len(self.inputQueue) != 0:
+					# assert False,"Still stuff in input queue"
 				self.setProgState('inputWasRead')
 				self.programCounter = self.programCounter + 2
 			elif currentOp[0] == 4:		# Output Operator
@@ -210,7 +237,7 @@ class CPU:
 				if debug_runCPU or debug_CPUOutput:
 					print("PC =",self.programCounter,"OUT Opcode = ",currentOp,end='')
 					print(" value =",val1)
-				outputQueue.append(val1)
+				self.outputQueue.append(val1)
 				self.programCounter = self.programCounter + 2
 				self.setProgState('outputReady')
 				return
@@ -218,36 +245,36 @@ class CPU:
 				if self.dealWithOp(currentOp,1) != 0:
 					self.programCounter = self.dealWithOp(currentOp,2)
 					if debug_runCPU:
-						print("PC =",self.programCounter,"JIT currentOp",currentOp,"Branch taken")
+						print("PC =",self.programCounter,"JIT Opcode = ",currentOp,"Branch taken")
 				else:
 					self.programCounter = self.programCounter + 3		
 					if debug_runCPU:
-						print("PC =",self.programCounter,"JIT currentOp",currentOp,"Branch not taken")
+						print("PC =",self.programCounter,"JIT Opcode = ",currentOp,"Branch not taken")
 			elif currentOp[0] == 6:		# Jump if false
 				if self.dealWithOp(currentOp,1) == 0:
 					self.programCounter = self.dealWithOp(currentOp,2)
 					if debug_runCPU:
-						print("PC =",self.programCounter,"JIT currentOp",currentOp,"Branch taken")
+						print("PC =",self.programCounter,"JIT Opcode = ",currentOp,"Branch taken")
 				else:
 					self.programCounter = self.programCounter + 3		
 					if debug_runCPU:
 						print("PC =",self.programCounter,"JIT currentOp",currentOp,"Branch not taken")
 			elif currentOp[0] == 7:		# Evaluate if less-than
 				valPair = self.evalOpPair(currentOp)
-				pos = programMemory[self.programCounter+3]
+				pos = self.programMemory[self.programCounter+3]
 				if valPair[0] < valPair[1]:
 					result = 1
 					if debug_runCPU:
-						print("PC =",self.programCounter,"ELT is",valPair[0],"less than =",valPair[1],"True")
+						print("PC =",self.programCounter,"ELT Opcode = ",currentOp,valPair[0],"less than =",valPair[1],"True")
 				else:
 					result = 0
 					if debug_runCPU:
-						print("PC =",self.programCounter,"ELT is",valPair[0],"less than =",valPair[1],"False")
-				self.writeOp3Result(currentOp,result)
+						print("PC =",self.programCounter,"ELT Opcode = ",currentOp,valPair[0],"less than =",valPair[1],"False")
+				self.writeOpResult(currentOp,3,result)
 				self.programCounter = self.programCounter + 4
 			elif currentOp[0] == 8:		# Evaluate if equal
 				valPair = self.evalOpPair(currentOp)
-				pos = programMemory[self.programCounter+3]
+				pos = self.programMemory[self.programCounter+3]
 				if valPair[0] == valPair[1]:
 					result = 1
 					if debug_runCPU:
@@ -256,20 +283,46 @@ class CPU:
 					result = 0
 					if debug_runCPU:
 						print("PC =",self.programCounter,"EEQ does",valPair[0],"equal =",valPair[1],"False")
-				self.writeOp3Result(currentOp,result)
+				self.writeOpResult(currentOp,3,result)
 				self.programCounter = self.programCounter + 4
 			elif currentOp[0] == 9:		# Sets relative base register value
 				if debug_runCPU:
-					print("PC =",self.programCounter,"SBR ",end='')
+					print("PC =",self.programCounter,"SBR Opcode = ",currentOp," ",end='')
 				self.relativeBaseRegister += self.dealWithOp(currentOp,1)
+				if debug_runCPU:
+					print("self.relativeBaseRegister =",self.relativeBaseRegister)
 				self.programCounter = self.programCounter + 2
 			elif currentOp[0] == 99:
+				if debug_runCPU:
+					print("PC =",self.programCounter,"END Opcode = ",currentOp)
 				self.progState = 'progDone'
 				return 'Done'
 			else:
+				print("PC =",self.programCounter,"END Opcode = ",currentOp)
 				print("error - unexpected opcode", currentOp[0])
 				exit()
 		assert False,"Unexpected exit of the CPU"
+
+	def loadIntCodeProgram(self):
+		""" 
+		"""
+		#global self.programMemory
+		#global self.inputQueue
+		#global self.outputQueue
+	#	debug_loadIntCodeProgram = True
+		debug_loadIntCodeProgram = False
+		# Load program memory from file
+		progName = "input.txt"
+		if debug_loadIntCodeProgram:
+			print("Input File Name :",progName)
+		with open(progName, 'r') as filehandle:  
+			inLine = filehandle.readline()
+			self.programMemory = map(int, inLine.split(','))
+		# pad out past end
+		for i in range(10000):
+			self.programMemory.append(0)
+		if debug_loadIntCodeProgram:
+			print(self.programMemory)
 
 north = 1
 south = 2
@@ -628,27 +681,11 @@ def findFillNeighbors(currentScanPoints,originalMaze):
 			newScanPoints.append([locY,locX-1])
 		if originalMaze[locY][locX+1] == ' ':
 			newScanPoints.append([locY,locX+1])
-	print("newScanPoints",newScanPoints)
+	#print("newScanPoints",newScanPoints)
 	return newScanPoints
-
-# Initialize queues
-inputQueue = []
-outputQueue = []
-
-# Load program memory from file
-progName = "input.txt"
-print("Input File Name :",progName)
-programMemory = []
-with open(progName, 'r') as filehandle:  
-	inLine = filehandle.readline()
-	programMemory = map(int, inLine.split(','))
-lenOfProgram=len(programMemory)
-for i in range(5000):
-	programMemory.append(0)
 
 # start up the CPU
 myCPU = CPU()
-myCPU.initCPU()
 
 currentLoc = [0,0]
 walls = []
@@ -669,11 +706,11 @@ while progStateVal != 'progDone':
 		print("main: currentLoc",currentLoc)
 		print("main: Moving",end='')
 		dirToText(moveDir)
-	inputQueue.append(moveDir)	
+	myCPU.inputQueue.append(moveDir)	
 	if debug_main:
-		print("main: inputQueue",inputQueue)
+		print("main: inputQueue",myCPU.inputQueue)
 	myCPU.runCPU()
-	if outputQueue[0] == 0:		# Hit a wall - did not move
+	if myCPU.outputQueue[0] == 0:		# Hit a wall - did not move
 		if debug_main:
 			print("\nmain: (wall) Hit a wall")
 		# Save wall
@@ -684,7 +721,7 @@ while progStateVal != 'progDone':
 			print("\nmain: (wall) moveDir (after direction change)",end='')
 			dirToText(moveDir)
 			print("main: (wall) done with wall")
-	elif outputQueue[0] == 1:	# Move was Ok
+	elif myCPU.outputQueue[0] == 1:	# Move was Ok
 		openBlockLocs.append(currentLoc)
 		if debug_main:
 			print("main: (OK) Move was OK")
@@ -703,7 +740,7 @@ while progStateVal != 'progDone':
 			print("main: (OK) openBlockLocs",openBlockLocs)
 			print("main: (OK) new moveDir",end='')
 			dirToText(moveDir)
-	elif outputQueue[0] == 2:	# Reached dest
+	elif myCPU.outputQueue[0] == 2:	# Reached dest
 		openBlockLocs.append(currentLoc)
 		if debug_main:
 			print("main: (OK) Move was OK")
@@ -721,7 +758,7 @@ while progStateVal != 'progDone':
 			dirToText(moveDir)
 		dest = currentLoc
 		#assert False,"main: Reached dest"
-	del outputQueue[0]
+	del myCPU.outputQueue[0]
 	if debug_main:
 		print("main: (loop) currentLoc",currentLoc)
 		print("main: (loop) openBlockLocs",openBlockLocs)
@@ -748,7 +785,7 @@ currentDir = west
 currentLoc = startLoc
 runMaze(currentLoc,currentDir,maze)
 
-dumpMaze(originalMaze)
+#dumpMaze(originalMaze)
 
 count = 0
 currentScanPoints = []
@@ -757,6 +794,8 @@ while currentScanPoints != []:
 	for point in currentScanPoints:
 		originalMaze[point[0]][point[1]] = 'o'
 	currentScanPoints = findFillNeighbors(currentScanPoints,originalMaze)
-	count += 1
-	print("count",count)
+	if currentScanPoints != []:
+		count += 1
+
+print("count",count)
 
