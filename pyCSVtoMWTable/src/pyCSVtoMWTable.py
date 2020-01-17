35226657a203972fedcfe30f6d9d6bddc6829f45
Doug Gilliland <doug@douglasgilliland.com>
2020-01-17 15:25:03 -0500

    py3 and tkinter

--- pyCSVtoMWTable/src/pyCSVtoMWTable.py
+++ pyCSVtoMWTable/src/pyCSVtoMWTable.py
@@ -23,6 +23,8 @@ Usage
 This program prompts for a path and file to select for input.
 The output file is created in the same path with a .MW output file name.
 
+Uses Python 3 and Tkinter.
+
 ===
 API
 ===
@@ -30,43 +32,30 @@ API
 """
 from __future__ import print_function
 
-from builtins import object
-import pygtk
-pygtk.require('2.0')
-
-import gtk
-
-# Check for new pygtk: this is new class in PyGtk 2.4
-if gtk.pygtk_version < (2,3,90):
-	 print("PyGtk 2.3.90 or later required")
-	 raise SystemExit
+from tkinter import filedialog
+from tkinter import *
+from tkinter import messagebox
 
 import csv
-import string
+#import string
 import os
 import sys
 
-#sys.path.append('C:\\Users\\doug_000\\Documents\\GitHub\\lb-Python-Code\\dgCommonModules')
-#sys.path.append('C:\\Users\\Douglas\\Documents\\GitHub\\lb-Python-Code\\dgCommonModules')
-sys.path.append('C:\\HWTeam\\Utilities\\dgCommonModules')
-sys.path.append('C:\\Users\\HPz420\\Documents\\GitHub\\land-boards\\lb-Python-Code\\dgCommonModules')
+sys.path.append('C:\\HWTeam\\Utilities\\dgCommonModules\\TKDGCommon')
+sys.path.append('C:\\Users\\HPz420\\Documents\\GitHub\\land-boards\\lb-Python-Code\\dgCommonModules\\TKDGCommon')
 
-from dgProgDefaults import *
-from dgReadCSVtoList import *
+from dgProgDefaultsTK import *
+from dgReadCSVtoListTK import *
 defaultPath = '.'
 
 from sys import argv
 
 def errorDialog(errorString):
-	"""
-	Prints an error message as a dialog box
-	"""
-	message = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK)
-	message.set_markup(errorString)
-	message.run()		# Display the dialog box and hang around waiting for the "OK" button
-	message.destroy()	# Takes down the dialog box
-	return
-	
+	messagebox.showerror("Error", errorString)
+
+def infoBox(msgString):
+	messagebox.showinfo("pyCSVtoMWTable",msgString)
+
 class ControlClass(object):
 	def theExecutive(self):
 		"""
@@ -91,16 +80,17 @@ class ControlClass(object):
 		fileToWrite = myCSVFileReadClass.getLastPathFileName()[0:-4] + '.MW'
 		#print 'fileToWrite',fileToWrite
 		try:
-			outFile = open(fileToWrite, 'wb')
+			outFile = open(fileToWrite, 'w')
 		except IOError:
 			errorDialog('ERROR - Cannot open the output file.\nIs the file already open?\nClose the file and return.')
 			try:
-				outFile = open(fileToWrite, 'wb')
+				outFile = open(fileToWrite, 'w')
 			except IOError:
 				errorDialog('ERROR - Tried again,  - Is the file already open?')
 				exit()
 		
 		self.writeOutMWTable(outFile, theInList)
+		infoBox("Finished")
 		
 	def writeOutMWTable(self, outFilePtr, theList):
 		"""
@@ -120,88 +110,26 @@ class ControlClass(object):
 			outFilePtr.write('|-\n')
 		outFilePtr.write('|}\n')
 
-class UIManager(object):
-	"""The UI manager
-	"""
-	interface = """
-	<ui>
-		<menubar name="MenuBar">
-			<menu action="File">
-				<menuitem action="Open"/>
-				<menuitem action="Quit"/>
-			</menu>
-			<menu action="Help">
-				<menuitem action="About"/>
-			</menu>
-		</menubar>
-	</ui>
-	"""
-
+class Dashboard:
 	def __init__(self):
-		"""Initialize the class
-		"""
-		# Create the top level window
-		window = gtk.Window()
-		window.connect('destroy', lambda w: gtk.main_quit())
-		window.set_default_size(200, 200)
-		
-		vbox = gtk.VBox()
-		
-		# Create a UIManager instance
-		uimanager = gtk.UIManager()
+		self.win = Tk()
+		self.win.geometry("320x240")
+		self.win.title("pyCSVtoMWTable")
 
-		# Add the accelerator group to the toplevel window
-		accelgroup = uimanager.get_accel_group()
-		window.add_accel_group(accelgroup)
-		window.set_title('pyCSVtoMWTable - Kicad Parts List creation program')
+	def add_menu(self):
+		self.mainmenu = Menu(self.win)
+		self.win.config(menu=self.mainmenu)
 
-		# Create an ActionGroup
-		actiongroup =	gtk.ActionGroup("pyCSVtoMWTable")
-		self.actiongroup = actiongroup
+		self.filemenu = Menu(self.mainmenu)
+		self.mainmenu.add_cascade(label="File",menu=self.filemenu)
 
-		# Create actions
-		self.actiongroup.add_actions([
-									("Open", gtk.STOCK_OPEN, "_Open", None, "Open an Existing Document", self.openIF),
-									("Quit", gtk.STOCK_QUIT, "_Quit", None, "Quit the Application", self.quit_application),
-									("File", None, "_File"),
-									("Help", None, "_Help"),
-									("About", None, "_About", None, "About pyCSVtoMWTable", self.about_pyCSVtoMWTable),
-									])
-		uimanager.insert_action_group(self.actiongroup, 0)
-		uimanager.add_ui_from_string(self.interface)
-		
-		menubar = uimanager.get_widget("/MenuBar")
-		vbox.pack_start(menubar, False)
-		
-		window.connect("destroy", lambda w: gtk.main_quit())
-		
-		window.add(vbox)
-		window.show_all()
+		self.filemenu.add_command(label="Open CSV PL file",command=control.theExecutive)
+		self.filemenu.add_separator()
+		self.filemenu.add_command(label="Exit",command=self.win.quit)
 
-	def openIF(self, b):
-		"""Open the interface by calling the control class
-		"""
-		myControl = ControlClass()
-		myControl.theExecutive()
+		self.win.mainloop()
 
-		message = gtk.MessageDialog(type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_OK)
-		message.set_markup("Conversion Complete")
-		message.run()		# Display the dialog box and hang around waiting for the "OK" button
-		message.destroy()	# Takes down the dialog box
-		return
-
-	def about_pyCSVtoMWTable(self, b):
-		"""The about dialog
-		"""
-		message = gtk.MessageDialog(type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_OK)
-		message.set_markup("About pyCSVtoMWTable\n(c) 2014 - Doug Gilliland\nAAC - All rights reserved\npyCSVtoMWTable Create a mediawiki table frm a CV file")
-		message.run()
-		message.destroy()
-		return
-
-	def quit_application(self, widget):
-		gtk.main_quit()
-
-if __name__ == '__main__':
-	ba = UIManager()
-	gtk.main()
+if __name__ == "__main__":
+	control = ControlClass()
+	x = Dashboard()
+	x.add_menu()
