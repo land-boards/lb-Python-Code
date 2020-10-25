ed89bd97d8bfa4dd92d25adc49a9517a28735af4
Doug Gilliland <doug@douglasgilliland.com>
2020-10-25 14:47:04 -0400

    d8 wip

--- AoC/AoC-2015/Day08/D08P1.py
+++ AoC/AoC-2015/Day08/D08P1.py
@@ -3,7 +3,20 @@ def readFileToList():
 	inList = []
 	with open('input.txt', 'r') as filehandle:  
 		for line in filehandle:
-			inLine = line.strip('\n')
-			inList.append(inLine)
+			inList.append(line.rstrip())
 	return inList
 
+def countStorageSpaceForLine(line):
+	return 0
+	
+# inList = readFileToList()
+inList = ['""','"abc"','"aaa\"aaa"','"\x27"']
+print(inList)
+count = 0
+for line in inList:
+	count += len(line)
+print("count of total chars",count)
+storageSpace = 0
+for line in inList:
+	storageSpace += countStorageSpaceForLine(line)
+	
