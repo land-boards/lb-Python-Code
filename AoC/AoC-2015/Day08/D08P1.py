8d9dca6c069267154df38421cbd04967a6bdb365
Doug Gilliland <doug@douglasgilliland.com>
2020-10-26 07:10:58 -0400

    d8 wip

--- AoC/AoC-2015/Day08/D08P1.py
+++ AoC/AoC-2015/Day08/D08P1.py
@@ -1,22 +1,57 @@
-#65535 is too hogh
+#985 is too low
 def readFileToList():
 	inList = []
-	with open('input.txt', 'r') as filehandle:  
+	with open('input1.txt', 'r', encoding='utf-8') as filehandle:  
 		for line in filehandle:
 			inList.append(line.rstrip())
 	return inList
 
-def countStorageSpaceForLine(line):
-	return 0
+def countStorageSpaceForLine(line,reallyLongString):
+	charCount = 0
+	localStr = ''
+	for charVal in line[1:-1]:
+		# print(charVal,end='')
+		charCount += 1
+		localStr += charVal
+	return(charCount,localStr)
 	
-# inList = readFileToList()
-inList = ['""','"abc"','"aaa\"aaa"','"\x27"']
-print(inList)
+inList = readFileToList()
+#inList = ['""','"abc"','"aaa\"aaa"','"\x27"']
+#print(inList)
 count = 0
 for line in inList:
 	count += len(line)
 print("count of total chars",count)
 storageSpace = 0
+reallyLongString = ''
 for line in inList:
-	storageSpace += countStorageSpaceForLine(line)
-	
+	pairOfThings = countStorageSpaceForLine(line,reallyLongString)
+	print("pairOfThings",pairOfThings)
+	storageSpace += pairOfThings[0]
+	reallyLongString += pairOfThings[1]
+print
+print("reallyLongString",reallyLongString)
+print("raw storageSpace",storageSpace)
+compCount = 0
+charToCheckOffset = 0
+while charToCheckOffset < len(reallyLongString):
+	print("Current char is :",reallyLongString[charToCheckOffset])
+	if reallyLongString[charToCheckOffset] == '\\':
+		print("Got backslash")
+		if (reallyLongString[charToCheckOffset+1] == 'x'):
+			print("Got x, Offset = 4")
+			charToCheckOffset += 4
+		elif reallyLongString[charToCheckOffset+1] == '\""':
+			print("Got backslash Offset = 2")
+			charToCheckOffset += 2
+		elif reallyLongString[charToCheckOffset+1] == '\\"':
+			print("Offset = 2")
+			charToCheckOffset += 2
+		else:
+			charToCheckOffset += 1
+	else:
+		charToCheckOffset += 1
+	compCount += 1
+compCount -= 1
+print("compCount",compCount)
+print("Delta =",count-compCount)
--- AoC/CommonFunctions/ReadTextFileLines.py
+++ AoC/CommonFunctions/ReadTextFileLines.py
@@ -2,8 +2,5 @@ def readFileToList():
 	inList = []
 	with open('input.txt', 'r') as filehandle:  
 		for line in filehandle:
-			inLine = line.strip('\n')
-			#inLine = inLine.split('x')
-			inList.append(inLine)
-	#print(inList)
+			inList.append(line.rstrip())
 	return inList
