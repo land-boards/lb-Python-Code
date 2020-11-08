ff4ac7f7d72e310a61ebaa281952839d4e397f3b
Doug Gilliland <doug@douglasgilliland.com>
2020-11-07 20:27:05 -0500

    Update D10P1.py

--- AoC/AoC-2015/Day10/D10P1.py
+++ AoC/AoC-2015/Day10/D10P1.py
@@ -1,8 +1,21 @@
 # D10P1
 
-InputStr = '1'
+def countDigits(strToCount):
+	firstChar = strToCount[0]
+	letterCount = 0
+	for consLtrCnt in range(len(strToCount)):
+		if strToCount[consLtrCnt] == firstChar:
+			letterCount += 1
+		else:
+			return letterCount
+	return letterCount
+	
 
-loops = 5
+InputStr = '1112'
 
-for loopCount in range(loops):
-	print("loopCount",loopCount)
+# loops = 5
+
+# for loopCount in range(loops):
+	# print("loopCount",loopCount)
+
+print(countDigits(InputStr))
