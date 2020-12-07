7ab45b1260fb5f4ad9e8da9f935056d3e7188d9d
Doug Gilliland <doug@douglasgilliland.com>
2020-12-07 05:56:26 -0500

    Update D20P1.py

--- AoC/AoC-2015/Day20/D20P1.py
+++ AoC/AoC-2015/Day20/D20P1.py
@@ -2,16 +2,30 @@
 
 import itertools
 
-presentAtHouseCount = []
-for elfNumber in itertools.count(1):
-	#print('elfNumber',elfNumber)
-	presentAtHouseCount.append(elfNumber*10)
+DEBUG_PRINT = True
+DEBUG_PRINT = False
+def debugPrint(thingToPrint):
+	if DEBUG_PRINT:
+		print(thingToPrint)
+
+presentAtHouseCount = 0
+maxCount = 0
+for elfNumber in itertools.count(776000,10):
+	#debugPrint('elfNumber' + str(elfNumber))
+	presentAtHouseCount = elfNumber * 10
 	for elf in range(elfNumber-1,0,-1):
-		#print('checking elf',elf,'against house number',elfNumber)
+		#debugPrint('checking elf' + str(elf) + 'against house number ' + str(elfNumber))
 		if elfNumber % elf == 0:
-			#print('elf',elf,'is modulo 0 at house',elfNumber)
-			presentAtHouseCount[elfNumber-1] += elf * 10
-			if presentAtHouseCount[elfNumber-1] >= 33100000:
-				break
-print(presentAtHouseCount)
-print(house)
+			#debugPrint('elf'+ str(elf) +'is modulo 0 at house ' + str(elfNumber))
+			presentAtHouseCount += elf * 10
+	if presentAtHouseCount >= 33100000:
+#	if presentAtHouseCount >= 130:
+		break
+	if presentAtHouseCount > maxCount:
+		maxCount = presentAtHouseCount
+	if (elfNumber % 100) == 0:
+		print(elfNumber,maxCount)
+		maxCount = 0
+	
+print(elfNumber,presentAtHouseCount)
+# print(house)
