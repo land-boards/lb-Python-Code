d4608dd51fc5fe216892231dd2b6acbb921b296f
Doug Gilliland <doug@douglasgilliland.com>
2020-12-10 14:26:46 -0500

    Update D10P1.py

--- AoC/AoC-2020/D10/D10P1.py
+++ AoC/AoC-2020/D10/D10P1.py
@@ -9,7 +9,7 @@
 # open file and read the content into an accumulated sum
 def readListOfInts():
 	newList = []
-	with open('input1.txt', 'r') as filehandle:  
+	with open('input.txt', 'r') as filehandle:  
 		for lineIn in filehandle:
 			newList.append(int(lineIn.strip()))
 	#print('newList',newList)
@@ -19,14 +19,20 @@ adapterJoltages = readListOfInts()
 #print('adapterJoltages',adapterJoltages)
 adapterJoltages.sort()
 print('adapterJoltages',adapterJoltages)
-countOfOnes = 0
-countOfThrees = 0
-for adapterOffset in range(1,len(adapterJoltages)-1):
+if adapterJoltages[0] == 1:
+	countOfOnes = 1
+	countOfThrees = 0
+else:
+	countOfOnes = 0
+	countOfThrees = 1
+for adapterOffset in range(0,len(adapterJoltages)-1):
 	if (adapterJoltages[adapterOffset+1] - adapterJoltages[adapterOffset]) == 1:
+		print('Diff of 1 from',adapterJoltages[adapterOffset],'to',adapterJoltages[adapterOffset+1])
 		countOfOnes += 1
 	if (adapterJoltages[adapterOffset+1] - adapterJoltages[adapterOffset]) == 3:
+		print('Diff of 3 from',adapterJoltages[adapterOffset],'to',adapterJoltages[adapterOffset+1])
 		countOfThrees += 1
-
+countOfThrees += 1
 print('countOfOnes',countOfOnes)
 print('countOfThrees',countOfThrees)
-	
+print('product',countOfOnes*countOfThrees)
