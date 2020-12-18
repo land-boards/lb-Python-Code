ca568212f0d277c2613b31a47f1c655891a2ddd9
Doug Gilliland <doug@douglasgilliland.com>
2020-12-18 07:38:55 -0500

    D17 Pt 1 solved

---
+++ AoC/AoC-2020/D17/D17p1.py
@@ -0,0 +1,133 @@
+""" 
+2020 D17 P1
+
+Inputs like:
+..##.......
+#...#...#..
+.#....#..#.
+
+Output:
+[['.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.'],
+['#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.'],
+['.', '#', '.', '.', '.', '.', '#', '.', '.', '#', '.']]
+
+112 is too low
+
+"""
+
+import copy
+
+DEBUG_PRINT = True
+DEBUG_PRINT = False
+
+def debugPrint(thingToPrint):
+	if DEBUG_PRINT:
+		print(thingToPrint)
+
+def readFileOfStringsToListOfLists():
+	inList = []
+	with open('input.txt', 'r') as filehandle:  
+		for line in filehandle:
+			inLine = line.rstrip()
+			inList.append(list(inLine))
+	return inList
+
+def countNeighbors(location,oldDict):
+	global DEBUG_PRINT
+	global neighborsOffsetList
+	neighborCount = 0
+	for neighbor in neighborsOffsetList:
+		checkLoc = (location[0]+neighbor[0],location[1]+neighbor[1],location[2]+neighbor[2])
+		if checkLoc in oldDict:
+			debugPrint('found # at (x,y,z) = ' + str(checkLoc))
+			neighborCount += 1
+	return neighborCount
+
+def makeNewDict(oldDict):
+	global DEBUG_PRINT
+	global neighborsOffsetList
+	#DEBUG_PRINT = True
+	debugPrint('(makeNewDict) : dict (before) = ' + str(oldDict))
+	newDict = {}
+	for location in oldDict:
+		debugPrint('\nchecking around location ' + str(location))
+		neighborCount = 0
+		neighborCount = countNeighbors(location,oldDict)
+		if (neighborCount == 2) or (neighborCount == 3):
+			if location not in newDict:
+				newDict[location] = '#'
+		neighborCount = 0		
+		for neighbor in neighborsOffsetList:
+			checkLoc = (location[0]+neighbor[0],location[1]+neighbor[1],location[2]+neighbor[2])
+			neighborCount = countNeighbors(checkLoc,oldDict)
+			debugPrint('Location at ' + str(location) + ' has ' + str(neighborCount) + ' neighbor(s)')
+			if (neighborCount == 3):
+				debugPrint('new active location ' + str(location) + ' has ' + str(neighborCount) + ' neighbors active ')
+				if checkLoc not in newDict:
+					debugPrint('adding neighbor ' + str(neighbor))
+					newDict[checkLoc] = '#'
+	debugPrint('newDict (after) is '+ str(newDict) + '\n')
+	DEBUG_PRINT = False
+	return newDict
+
+def printDict(arrayDict):
+	xMax, xMin, yMax, yMin, zMax, zMin = 0,0,0,0,0,0
+	for point in arrayDict:
+		if point[0] > xMax:
+			xMax = point[0]
+		if point[0] < xMin:
+			xMin = point[0]
+		if point[1] > yMax:
+			yMax = point[1]
+		if point[1] < yMin:
+			yMin = point[1]
+		if point[2] > zMax:
+			zMax = point[2]
+		if point[2] < zMin:
+			zMin = point[2]
+	#print('max/min x,y,z',xMax, xMin, yMax, yMin, zMax, zMin)
+	for z in range(zMin,zMax+1):
+		for y in range(yMin,yMax+1):
+			for x in range(xMin,xMax+1):
+				if (x,y,z) in arrayDict:
+					print('#',end='')
+				else:
+					print('.',end='')
+			print()
+		print()
+	print(point)
+
+# program
+inList = readFileOfStringsToListOfLists()
+debugPrint(str(inList))
+
+# list of offsets to all possible neighbors
+neighborsOffsetList = []
+for z in range(-1,2,1):
+	debugPrint(z)
+	for y in range(-1,2,1):
+		for x in range(-1,2,1):
+			if (x,y,z) != (0,0,0):
+				neighborsOffsetList.append((x,y,z))
+debugPrint('neighborsOffsetList' + str(neighborsOffsetList))
+debugPrint(str(len(neighborsOffsetList)))
+
+z = 0
+conroySpaceDict = {}
+for y in range(len(inList)):
+	for x in range(len(inList[0])):
+		debugPrint(str(inList[y][x]))
+		if inList[y][x] == '#':
+			conroySpaceDict[(x,y,z)] = '#'
+	debugPrint('')
+debugPrint(str(conroySpaceDict))
+for dictItem in conroySpaceDict:
+	debugPrint(str(dictItem))
+printDict(conroySpaceDict)
+for loopCount in range(6):
+	print(loopCount)
+	debugPrint(str(loopCount))
+	conroySpaceDict = copy.deepcopy(makeNewDict(conroySpaceDict))
+	printDict(conroySpaceDict)
+	print('filled count',len(conroySpaceDict))
+debugPrint(str(conroySpaceDict))
