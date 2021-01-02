322fe8b9ee0ba449e1d1ec385d284ba73628950d
Doug Gilliland <doug@douglasgilliland.com>
2021-01-02 17:00:40 -0500

    Update D23P2.py

--- AoC/AoC-2020/D23/D23P2.py
+++ AoC/AoC-2020/D23/D23P2.py
@@ -1,6 +1,5 @@
 import time
 
-# 94365872 too high
 
 # At start
 startTime = time.time()
@@ -14,95 +13,85 @@ def debugPrint(thingToPrint):
 	global DEBUG_PRINT		# need to put in each function
 	if DEBUG_PRINT:
 		print(thingToPrint)
-
-def getCups():
-	global q
 	
-# inStr = '284573961'		# My input
-inStr = '389125467'	# Example
-moves = 1000				# Number of moves
+def fillInDictFromList():
+	# Initalize the input list
+	global inDict
+	global inStr
+	for element in range(len(inStr)-1):
+		inDict[int(inStr[element])] = int(inStr[element+1])
+	inDict[int(inStr[-1])] = int(inStr[0])
+	return int(inStr[0]), int(inStr[-1])
 
-# Initalize the queue
-q = []
-for element in inStr:
-	q.append(int(element))
-for i in range(10,1000001):
-	q.append(i)
-debugPrint('initialized q last element' + str(q[1000000-1]))
+def printTenFromOne(space=False,ct=9):
+	global inDict
+	global DEBUG_PRINT
+	val = 1
+	if DEBUG_PRINT:
+		print(val,end = ' ')
+	for _ in range(ct):
+		val = inDict[val]
+		if space:
+			print(val,end = ' ')
+		else:
+			print(val,end = ' ')
+	print()
+
+def doMove(currentVal):
+	global maxVal
+	pickupList = []
+	pickupList.append(inDict[currentVal])
+	pickupList.append(inDict[pickupList[-1]])
+	pickupList.append(inDict[pickupList[-1]])
+	# debugPrint('pick up: ' + str(pickupList))
+	nextVal = inDict[pickupList[-1]]
+	afterLoc = inDict[inDict[pickupList[-1]]]
+	insertLoc = currentVal - 1
+	if insertLoc == 0:
+		insertLoc = maxVal
+	# debugPrint('insertLoc (before) = ' + str(insertLoc))
+	while insertLoc in pickupList:
+		insertLoc -= 1
+		if insertLoc == 0:
+			insertLoc = maxVal
+	# debugPrint('destination: ' + str(insertLoc))
+	endLoc = inDict[insertLoc]
+	# debugPrint('  currentVal = ' + str(currentVal))
+	# debugPrint('  next val   = ' + str(nextVal))
+	# debugPrint('  afterLoc   = ' + str(afterLoc))
+	inDict[insertLoc] = pickupList[0]
+	inDict[pickupList[2]] = endLoc
+	inDict[currentVal] = nextVal
+	return nextVal
+	
+inStr = '284573961'		# My input
+# inStr = '389125467'	# Example
+moves = 10_000_000				# Number of moves
+maxVal = 1_000_000
+
+inDict = {}
+currentVal,lastVal = fillInDictFromList()
+print('first val',currentVal,'last val',lastVal)
+# printTenFromOne(True,maxVal)
+for num in range(10,1_000_001):
+	inDict[num] = num + 1
+inDict[1_000_000] = currentVal
+inDict[lastVal] = 10
+# printTenFromOne(True,10)
+
+endTime = time.time()
+print('after creating dict time',endTime-startTime)
+	
+# debugPrint('currentVal ' + str(currentVal))
+for move in range(moves):
+	# if move % 100_000 == 0:
+		# print(move)
+	# debugPrint('\n--- move '+ str(move+1) + ' ---')
+	# if DEBUG_PRINT:
+		# printTenFromOne()
+	currentVal = doMove(currentVal)
+printTenFromOne()
+
 endTime = time.time()
 print('time',endTime-startTime)
-numberOfCups = len(q)
-debugPrint('Number of cups ' + str(numberOfCups))
-debugPrint('Number of moves ' + str(moves) + '\n')
-# debugPrint('Initial cups list ' + str(q))
-
-# Setup
-moveNumber = 1
-currentCupPtr = 0
-
-# The loop
-while moveNumber <= moves:
-	print('-- Move ' + str(moveNumber) + ' --')
-	debugPrint('cups: ' + str(q))
-	cupsRemovedList = []
-	nextCupNumber = q[(currentCupPtr + 4) % numberOfCups]
-	debugPrint('nextCupNumber ' + str(nextCupNumber))
-	for i in range(3):
-		getCupOffset = (currentCupPtr + 1 + i) % numberOfCups
-		debugPrint('getCupOffset ' + str(getCupOffset))
-		cupsRemovedList.append(q[getCupOffset])
-	debugPrint('pick up: ' + str(cupsRemovedList))
-	lookingForValue = q[currentCupPtr] - 1
-	if lookingForValue == 0:
-		lookingForValue = numberOfCups
-	debugPrint('Initial destination cup ' + str(lookingForValue))
-	for cupToRemove in cupsRemovedList:
-		q.remove(cupToRemove)
-	debugPrint('q after removal ' + str(q))
-	if lookingForValue in cupsRemovedList:
-		while lookingForValue in cupsRemovedList:
-			debugPrint('new looking for value ' + str(lookingForValue))
-			lookingForValue -= 1
-			if lookingForValue == 0:
-				lookingForValue = numberOfCups
-		debugPrint('found val ' + str(lookingForValue))
-		debugPrint('destination: ' + str(lookingForValue))
-		for offset in range(len(q)):
-			if q[offset] == lookingForValue:
-				break
-		offset += 1
-		debugPrint('insert at offset ' + str(offset))
-	else:
-		# assert False,'wtf'
-		debugPrint('destination:  ' + str(lookingForValue))
-		if lookingForValue == 0:
-			lookingForValue = numberOfCups
-		for offset in range(len(q)):
-			if q[offset] == lookingForValue:
-				break
-		offset += 1
-		debugPrint('insert at offset ' + str(offset))
-	debugPrint('inserting cups ' + str(cupsRemovedList))
-	for val in range(3):
-		q.insert(offset,cupsRemovedList[2-val])	# insert backwards
-	currentCupPtr = (currentCupPtr + 1) % numberOfCups
-	# rotate left until lines up
-	while q[currentCupPtr] != nextCupNumber:
-		q = q[1:] + q[:1]
-	moveNumber += 1
-	debugPrint('')
-	endTime = time.time()
-	print('time',endTime-startTime)
-	off = 0
-	while q[off] != 1:
-		off += 1
-	print('vals',q[off%1000000],q[(off+1)%1000000],q[(off+2)%1000000])
-	print('q after move',q[0:10])
-off = 0
-while q[off] != 1:
-	off += 1
-endTime = time.time()
-print('time',endTime-startTime)
-endTime = time.time()
-print('time',endTime-startTime)
-input()
+# input('enter to exit')
