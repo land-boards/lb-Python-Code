06a0eff336bd353706f5ea4fd0a69d9f9cbaf71c
Doug Gilliland <doug@douglasgilliland.com>
2021-01-03 07:51:16 -0500

    Update D22P1.py

--- AoC/AoC-2020/D22/D22P1.py
+++ AoC/AoC-2020/D22/D22P1.py
@@ -4,71 +4,73 @@ D22P1
 
 """
 
-list1 = []
-list2 = []
+import collections
+
+playa1Deque = collections.deque([])
+playa2Deque = collections.deque([])
 
 def readToLists(fileName):
+	""" Cards are added to the right in the deque
 	"""
-	"""
-	global list1
-	global list2
+	global playa1Deque
+	global playa2Deque
 	stateList = ['inWaitPlayer1','inList1','inWaitPlayer2','inList2']
 	state = 'inWaitPlayer1'
 	with open(fileName, 'r') as filehandle:  
 		for line in filehandle:
 			line = line.strip()
 			if state == 'inWaitPlayer1':
-				# print('Playa 1')
 				state = 'inList1'
 			elif state == 'inList1':
 				if line != '':
-					list1.append(int(line.strip()))
+					playa1Deque.append(int(line.strip()))
 				else:
 					state = 'inWaitPlayer2'
 			elif state == 'inWaitPlayer2':
-				# print('Playa 2')
 				state = 'inList2'
 			elif state == 'inList2':
-				list2.append(int(line.strip()))
+				playa2Deque.append(int(line.strip()))
 	return
 
-readToLists('input.txt')
-# print('start list')
-# print(list1)
-# print(list2)
-
-while (len(list1) != 0) and (len(list2) != 0):
-	topCardPlaya1 = list1[0]
-	topCardPlaya2 = list2[0]
-	# print('comparing',topCardPlaya1,'to',topCardPlaya2)
-	if topCardPlaya1 > topCardPlaya2:
-		list1.append(topCardPlaya1)
-		list1.append(topCardPlaya2)
-		list1.pop(0)
-		list2.pop(0)
-		# print('Playa 1 wins')
+def calculateResult():
+	global playa1Deque
+	global playa2Deque
+	productSum = 0
+	if len(playa1Deque) != 0:
+		# print('Playa 1 deque',playa1Deque)
+		val = len(playa1Deque)
+		for listItem in playa1Deque:
+			productSum += val*listItem
+			val -= 1
 	else:
-		list2.append(topCardPlaya2)
-		list2.append(topCardPlaya1)
-		list1.pop(0)
-		list2.pop(0)	
-		# print('Playa 2 wins')
-	# print('next list')
-	# print(list1)
-	# print(list2)
+		# print('Playa 2 deque',playa2Deque)
+		val = len(playa2Deque)
+		for listItem in playa2Deque:
+			productSum += val*listItem
+			val -= 1
+	return productSum
 
-productSum = 0
-if len(list1) != 0:
-	print('Playa 1',list1)
-	val = len(list1)
-	for listItem in list1:
-		productSum += val*listItem
-		val -= 1
-else:
-	print('Playa 2',list2)
-	val = len(list2)
-	for listItem in list2:
-		productSum += val*listItem
-		val -= 1
+def playRound(p1Deque,p2Deque):
+	"""
+	Cards are removed from the left and added to the right of the deck
+	"""
+	topCardPlaya1 = p1Deque.popleft()
+	topCardPlaya2 = p2Deque.popleft()
+	if topCardPlaya1 > topCardPlaya2:
+		p1Deque.append(topCardPlaya1)
+		p1Deque.append(topCardPlaya2)
+	else:
+		p2Deque.append(topCardPlaya2)
+		p2Deque.append(topCardPlaya1)
+	if ((len(p1Deque) == 0) or (len(p2Deque) == 0)):
+		return True
+	return False
+
+# Program follows
+readToLists('input.txt')
+
+while not playRound(playa1Deque,playa2Deque):
+	pass
 	
+productSum = calculateResult()
 print('productSum',productSum)
