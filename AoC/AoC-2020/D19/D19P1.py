ddd3960f9ec12634e0accca885f04a317c7a2ff6
Doug Gilliland <doug@douglasgilliland.com>
2021-01-04 14:39:31 -0500

    Update D19P1.py

--- AoC/AoC-2020/D19/D19P1.py
+++ AoC/AoC-2020/D19/D19P1.py
@@ -9,48 +9,78 @@ def debugPrint(thingToPrint):
 	if DEBUG_PRINT:
 		print(thingToPrint)
 
-def readFileOfStringsToListOfLists(inFileName):
+def readFileOfStringsToList(inFileName):
 	global DEBUG_PRINT		# need to put in each function
 	inList = []
 	with open(inFileName, 'r') as filehandle:  
 		for line in filehandle:
 			inLine = line.rstrip()
 			inList.append(inLine)
-	debugPrint('(readFileOfStringsToListOfLists): inList - ' + str(inList))
+	debugPrint('(readFileOfStringsToList): inList - ' + str(inList))
 	return inList
 
 def formatInputList(inList):
 	global DEBUG_PRINT		# need to put in each function
 	global unsolvedDict
 	global solvedDict
+	global testStrings
 	# Input = ['0: 4 1 5', '1: 2 3 | 3 2', '3: 4 5 | 5 4', 
 	# Output = 
+	state = 'inRules'
 	for line in inList:
-		if ':' in line:
+		if line == '':
+			state = 'inTestData'
+		elif state == 'inRules':
 			line = line.replace(' ', ',')
 			line = line.replace(':', '')
 			line = line.replace('"', '')
 			sp = line.split(',')
 			print('sp',sp)
+			newRule = []
+			newLine = []
 			if len(sp) == 2:
-				solvedDict[int(sp[0])] = sp[1]
-			elif len(sp) == 3:
-				unsolvedDict[int(sp[0])] = [[int(sp[1]),int(sp[2])]]
-			elif len(sp) == 4:
-				unsolvedDict[int(sp[0])] = [[int(sp[1]),int(sp[2])]]
-			elif len(sp) == 6:
-				unsolvedDict[int(sp[0])] = [[int(sp[1]),int(sp[2])],[int(sp[4]),int(sp[5])]]
+				solvedDict[sp[0]] = sp[1]
 			else:
-				print('wtf-1')
-		elif line != '':
-			break
+				off = 1
+				while off < len(sp):
+					if sp[off] == '|':
+						newRule.append(newLine)
+					else:
+						newLine.append(int(sp[off]))
+					off += 1
+				newRule.append(newLine)
+				unsolvedDict[int(sp[0])] = newRule
+		elif state == 'inTestData':
+			testStrings.append(line)
+		else:
+			print('wtf-1')
 	return
 
+def checkAllValsForSolved(checkRule):
+	# unsolvedDict = {0: [[4, 1]], 1: [[2, 3], [3, 2]], 3: [[4, 5], [5, 4]], 2: [[4, 4], [5, 5]]}
+	global DEBUG_PRINT		# need to put in each function
+	global unsolvedDict
+	global solvedDict
+	ruleAsList = unsolvedDict[checkRule]
+	varsInRule = []
+	
+
+def findNextSolvableRule():
+	global DEBUG_PRINT		# need to put in each function
+	global unsolvedDict
+	global solvedDict
+	for rule in unsolvedDict:
+		if checkAllValsForSolved(unsolvedDict[rule]):
+			print('all vals are solved',unsolvedDict[rule])
+		else:
+			print('all vals are not solved',unsolvedDict[rule])
+
 # Program start
-inList = readFileOfStringsToListOfLists('input1.txt')
-print(inList)
+inList = readFileOfStringsToList('input1.txt')
+# print(inList)
 solvedDict = {}
 unsolvedDict = {}
+testStrings = []
 
 formatInputList(inList)
 DEBUG_PRINT = True
