48a0185cc49bfa7b600133eb84062051f2ab356b
Doug Gilliland <doug@douglasgilliland.com>
2021-12-03 17:03:43 -0500

    Update D03P2.py

--- AoC/AoC-2021/D03/D03P2.py
+++ AoC/AoC-2021/D03/D03P2.py
@@ -4,6 +4,7 @@
 # Part 2
 
 """
+readFileToListOfStrings()
 """
 # readFileToListOfStrings
 def readFileToListOfStrings(fileName):
@@ -15,32 +16,65 @@ def readFileToListOfStrings(fileName):
 			inList.append(line.rstrip())
 	return inList
 
-inList = readFileToListOfStrings('input1.txt')
-print(inList)
+def bitsCount(checkList,checkBit):
+	onesCount = 0
+	zerosCount = 0
+	print("bitsCount: checkList",checkList)
+	for valToCheck in checkList:
+		if valToCheck[checkBit] == '1':
+			onesCount += 1
+		if valToCheck[checkBit] == '0':
+			zerosCount += 1
+	return onesCount, zerosCount
+
+def removeVals(myList,bitChk,bitVal):
+	retList = []
+	for item in myList:
+		# print("bitChk ",bitChk)
+		# print("item[bitChk] ",item[bitChk])
+		# print("bitVal ",bitVal)
+		if item[bitChk] != bitVal:
+			retList.append(item)
+		else:
+			print("removing ",item)
+	print("removeVals: retList =",retList)
+	return retList
+
+inList = readFileToListOfStrings('input.txt')
+print("inList",inList)
 for row in inList:
 	print(row)
 numberOfBits = len(inList[0])
 print("numberOfBits",numberOfBits)
-zeroBitsCount = []
-oneBitsCount = []
-for bitNum in range(numberOfBits):
-	onesCount = 0
-	zerosCount = 0
-	for row in inList:
-		if row[bitNum] == '1':
-			onesCount += 1
-		else:
-			zerosCount += 1
-	zeroBitsCount.append(zerosCount)
-	oneBitsCount.append(onesCount)
-print("zeroBitsCount",zeroBitsCount)
-print("oneBitsCount",oneBitsCount)
-oxygenArray = []
-for bitNum in range(numberOfBits):
-	if zeroBitsCount[bitNum] > oneBitsCount[bitNum]:
-		oxygenArray.append('0')
-	elif zeroBitsCount[bitNum] < oneBitsCount[bitNum]:
-		oxygenArray.append('1')
-	else:
-		oxygenArray.append('X')
-print("oxygenArray",oxygenArray)
+
+oxyList = list(inList)
+for checkBit in range(numberOfBits):
+	onesCount, zerosCount = bitsCount(oxyList,checkBit)
+	print("1s 0s =",onesCount, zerosCount)
+	if len(oxyList) == 1:
+		break
+	if onesCount < zerosCount:
+		oxyList = removeVals(oxyList,checkBit,'1')
+	elif onesCount > zerosCount:
+		oxyList = removeVals(oxyList,checkBit,'0')
+	elif onesCount == zerosCount:
+		oxyList = removeVals(oxyList,checkBit,'0')
+
+co2List = list(inList)
+for checkBit in range(numberOfBits):
+	onesCount, zerosCount = bitsCount(co2List,checkBit)
+	print("1s 0s =",onesCount, zerosCount)
+	if len(co2List) == 1:
+		break
+	if onesCount < zerosCount:
+		co2List = removeVals(co2List,checkBit,'0')
+	elif onesCount > zerosCount:
+		co2List = removeVals(co2List,checkBit,'1')
+	elif onesCount == zerosCount:
+		co2List = removeVals(co2List,checkBit,'1')
+
+print("oxyList",oxyList)
+print("co2List",co2List)
+oxyNum = int(oxyList[0],2)
+co2Num = int(co2List[0],2)
+print("product",oxyNum*co2Num)
