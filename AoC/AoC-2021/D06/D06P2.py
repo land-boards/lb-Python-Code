8b6dc38909e7864039bc26cd6d3894d825e57527
Doug Gilliland <doug@douglasgilliland.com>
2021-12-06 06:04:52 -0500

    Create D06P2.py

---
+++ AoC/AoC-2021/D06/D06P2.py
@@ -0,0 +1,27 @@
+# D06P1.py
+# 2021 Advent of Code
+# Day 6
+# Part 2
+
+inList = [5,3,2,2,1,1,4,1,5,5,1,3,1,5,1,2,1,4,1,2,1,2,1,4,2,4,1,5,1,3,5,4,3,3,1,4,1,3,4,4,1,5,4,3,3,2,5,1,1,3,1,4,3,2,2,3,1,3,1,3,1,5,3,5,1,3,1,4,2,1,4,1,5,5,5,2,4,2,1,4,1,3,5,5,1,4,1,1,4,2,2,1,3,1,1,1,1,3,4,1,4,1,1,1,4,4,4,1,3,1,3,4,1,4,1,2,2,2,5,4,1,3,1,2,1,4,1,4,5,2,4,5,4,1,2,1,4,2,2,2,1,3,5,2,5,1,1,4,5,4,3,2,4,1,5,2,2,5,1,4,1,5,1,3,5,1,2,1,1,1,5,4,4,5,1,1,1,4,1,3,3,5,5,1,5,2,1,1,3,1,1,3,2,3,4,4,1,5,5,3,2,1,1,1,4,3,1,3,3,1,1,2,2,1,2,2,2,1,1,5,1,2,2,5,2,4,1,1,2,4,1,2,3,4,1,2,1,2,4,2,1,1,5,3,1,4,4,4,1,5,2,3,4,4,1,5,1,2,2,4,1,1,2,1,1,1,1,5,1,3,3,1,1,1,1,4,1,2,2,5,1,2,1,3,4,1,3,4,3,3,1,1,5,5,5,2,4,3,1,4]
+inList1 = [3,4,3,1,2]
+# 6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8
+# 6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8
+day = 0
+numberOfDays = 100
+workingList = inList1
+print("day",day,workingList)
+dayFishList = []
+while day < numberOfDays:
+	lenList = len(workingList)
+	for item in range(lenList):
+		if workingList[item] == 0:
+			workingList.append(8)
+			workingList[item] = 6
+		else:
+			workingList[item] = workingList[item] - 1
+	day += 1
+	print("day",day,"number of fish",len(workingList))
+	dayFishList.append(len(workingList))
+print("Number of fish",len(workingList))
+print("dayFishList",dayFishList)
