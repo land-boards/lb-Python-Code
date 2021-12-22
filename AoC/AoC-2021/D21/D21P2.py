0b31b8de5108d07f872044e15aa2ab36fe5c60c9
Doug Gilliland <doug@douglasgilliland.com>
2021-12-22 06:05:46 -0500

    AoC

--- AoC/AoC-2021/D21/D21P2.py
+++ AoC/AoC-2021/D21/D21P2.py
@@ -1,11 +1,11 @@
-# D21P1.py
+# D21P2.py
 # 2021 Advent of Code
 # Day 21
-# Part 1
+# Part 2
 
 def rollDice(diceVal):
 	diceVal += 1
-	if diceVal > 100:
+	if diceVal > 3:
 		diceVal = 1
 	return diceVal
 
@@ -28,37 +28,38 @@ def movePlayer(playerPosition,moves):
 	return newPlayerPosition
 
 Player1CurrentPosition = 4
-Player2CurrentPosition = 3
+Player2CurrentPosition = 8
 diceLastRollValue = 0
 Player1Score = 0
 Player2Score = 0
 rollCount = 0
 lowScore = 0
+endScore = 21
 
-while (Player1Score < 1000) and (Player2Score < 1000):
+while (Player1Score < endScore) and (Player2Score < endScore):
 	# Run Player 1
-	print("\nPlayer 1",end = '')
+	# print("\nPlayer 1",end = '')
 	nextDiceRolls = rollDice3Times(diceLastRollValue)
 	diceLastRollValue = nextDiceRolls[-1]
-	print(" rolls",nextDiceRolls,end=' ')
+	# print(" rolls",nextDiceRolls,end=' ')
 	Player1CurrentPosition = movePlayer(Player1CurrentPosition,nextDiceRolls)
-	print("and moves to space",Player1CurrentPosition,end='')
+	# print("and moves to space",Player1CurrentPosition,end='')
 	Player1Score += Player1CurrentPosition
-	print(" for a total score",Player1Score)
+	# print(" for a total score",Player1Score)
 	rollCount += 3
-	if Player1Score < 1000:
+	if Player1Score < endScore:
 		lowScore = Player1Score
 
-	if Player1Score < 1000:
+	if Player1Score < endScore:
 		# Run Player 2
-		print("Player 2",end = '')
+		# print("Player 2",end = '')
 		nextDiceRolls = rollDice3Times(diceLastRollValue)
 		diceLastRollValue = nextDiceRolls[-1]
-		print(" rolls",nextDiceRolls,end=' ')
+		# print(" rolls",nextDiceRolls,end=' ')
 		Player2CurrentPosition = movePlayer(Player2CurrentPosition,nextDiceRolls)
-		print("and moves to space",Player2CurrentPosition,end='')
+		# print("and moves to space",Player2CurrentPosition,end='')
 		Player2Score += Player2CurrentPosition
-		print(" for a total score",Player2Score)
+		# print(" for a total score",Player2Score)
 		rollCount += 3
 		if Player2Score < 1000:
 			lowScore = Player2Score
